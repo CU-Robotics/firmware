70173814081dc35ccda0c53566bcf6a4664ba329
StudentKyle <studentkyles@gmail.com>
2023-09-30 15:43:18 -0600

    Adds basic behavior for the ICM20649 into an object structure.

--- src/sensors/imu-sensor/ICM20649.cpp
+++ src/sensors/imu-sensor/ICM20649.cpp
@@ -1,14 +1,56 @@
-#include <Arduino.h> // https://www.arduino.cc/reference/en/
-#include <Wire.h> // https://www.arduino.cc/reference/en/language/functions/communication/wire/
-
 #include <ICM20649.hpp>
 
 ICM20649::ICM20649() {
-    
+    // initialize ICM
+
+    // start I2C communication 
+    sensor.begin_I2C();
+
+    // set data ranges
+    sensor.setAccelRange(ICM20649_ACCEL_RANGE_30_G)
+    sensor.setGyroRange(ICM20649_GYRO_RANGE_4000_DPS)
+
+    // set accel rate (via divisor setting)
+    sensor.setAccelRateDivisor(4095);
+    accel_rate = get_accel_data_rate();
+
+    // set gyro rate (via divisor setting)
+    sensor.setGyroRateDivisor();
+    gyro_rate = get_gyro_data_rate();
 }
 
-void IMUSensor::read_accel() {
-    // I2C on SDA and SCL pins.
-    // Serial.digitalRead(pin)
+float get_temperature() { return temperature; };
 
+float get_accel_X() { return accel_X};
+float get_accel_Y() { return accel_X};
+float get_accel_Z() { return accel_X};
+
+float get_gyro_X() { return gyro_X; };
+float get_gyro_Y() { return gyro_Y; };
+float get_gyro_Z() { return gyro_Z; };
+
+void ICM20649::read() {
+    // get the event data from the sensor class
+    sensor.getEvent(&accel, &gyro, &temp);
+
+    // assign result to this object's members.
+    // (could increase efficiency by specifying which values we need, and only assigning values from that. However, getEvent will read all values from the sensor regardless)
+    accel_X = accel.accel.x;
+    accel_Y = accel.accel.y;
+    accel_Z = accel.accel.z;
+    gyro_X = gryo.gyro.x;
+    gyro_Y = gryo.gyro.y;
+    gyro_Z = gryo.gyro.z;
+
+    temp = temp.temperature;
+}
+
+// calculate the approximate acceleration rate (Hz) from the divisor.
+float ICM20649::get_accel_data_rate() {
+    // equation from Adafruit ICM20649 example code
+    return 1125 / (1.0 + sensor.getAccelRateDivisor());
+}
+
+float ICM20649::get_gyro_data_rate() {
+    return gyro_rate = 1100 / (1.0 + sensor.getGyroRateDivisor());
 }
--- src/sensors/imu-sensor/ICM20649.hpp
+++ src/sensors/imu-sensor/ICM20649.hpp
@@ -1,3 +1,7 @@
+#include <Arduino.h> // https://www.arduino.cc/reference/en/
+#include <Wire.h> // https://www.arduino.cc/reference/en/language/functions/communication/wire/
+// adafruit library specific to ICM20(...) hardware
+#include <Adafruit_ICM20X.h> // https://adafruit.github.io/Adafruit_ICM20X/html/class_adafruit___i_c_m20_x.html
 
 #ifndef ICM20649_H
 #define ICM20649_H
@@ -6,24 +10,41 @@ class IMU20649{
 public:
     IMU20649();
     
-    void read_accel();
-    void read_gyro();
+    void read();
 
-    float get_temperature() { return temperature; };
+    float get_temperature();
     
     float get_accel_X();
     float get_accel_Y();
     float get_accel_Z();
 
-    float get_gyro_X() { return gyro_X; };
-    float get_gyro_Y() { return gyro_Y; };
-    float get_gyro_Z() { return gyro_Z; };
+    float get_gyro_X();
+    float get_gyro_Y();
+    float get_gyro_Z();
 
 private:
+    // sensor object from adafruit libraries.
+    Adafruit_ICM20649 sensor;
+    // sensor events to read from
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+
+    // calculate the approximate acceleration  rates in Hz from the divisor.
+    float get_accel_data_rate();
+    // calculate the approximate gyroscope rates in Hz from the divisor.
+    float get_gyro_data_rate();
+
+    
+    float accel_rate; // approximate acceleration data rate (Hz) calculated from divisor. 
+
+    // acceleration values assigned after read()
     float accel_X = 0;
     float accel_Y = 0;
     float accel_Z = 0;
 
+    float gyro_rate; // approximate gyroscope data rate (Hz) calculated from divisor.
+    // gyroscope values assigned after read()
     float gyro_X = 0;
     float gyro_Y = 0;
     float gyro_Z = 0;
