bd51255946b7a573748aa68e458d51ebb7f38d60
Jackson Stepka <jvsdestroyer25@gmail.com>
2023-10-02 13:14:59 -0600

    Added doccumentation and cleaned up some of Serial5 operations
    Still need to incorporate keyboard and mouse mappings.

--- DR16/DR16.cpp
+++ DR16/DR16.cpp
@@ -1,94 +1,100 @@
 #include "DR16.hpp"
 
-
-
 DR16::DR16() {}
 
 void DR16::Init()
 {
-    Serial5.begin(100000, SERIAL_8E1_RXINV_TXINV);
-    Serial5.flush();
-    Serial5.clear();
+	// start Serial5 HardwareSerial with 1
+	Serial5.begin(100000, SERIAL_8E1_RXINV_TXINV);
+	// await any active writing and clear the buffer
+	Serial5.flush();
+	Serial5.clear();
 
-    for (int i = 0; i < DR16_PACKET_SIZE; i++)
-    {
-        m_inputRaw[i] = 0;
-    }
+	// init all input buffers to 0
+	for (int i = 0; i < DR16_PACKET_SIZE; i++)
+	{
+		m_inputRaw[i] = 0;
+	}
 
-    for (int i = 0; i < DR16_INPUT_VALUE_COUNT; i++)
-    {
-        m_input[i] = 0;
-    }
+	for (int i = 0; i < DR16_INPUT_VALUE_COUNT; i++)
+	{
+		m_input[i] = 0;
+	}
 }
 
 void DR16::Read()
 {
-    uint16_t c0{ 0 }, c1{ 0 }, c2{ 0 }, c3{ 0 }, wheel{ 0 };
-    uint8_t s1{ 0 }, s2{ 0 };
+	// each channel is 11 bits, minus the switches
+	uint16_t c0{ 0 }, c1{ 0 }, c2{ 0 }, c3{ 0 }, wh{ 0 };
+	uint8_t s1{ 0 }, s2{ 0 };
 
-    // read
-    if (Serial5.available() > 18)
-    {
-      Serial5.clear();
-      return;
-    }
-    if (Serial5.available() < 18)
-    {
-      return;
-    }
-    
-    Serial5.flush();
-    Serial5.readBytes(m_inputRaw, 18);
-    Serial5.clear();
+	// verify that the receiver is ready to be read
+	// since each packet it sends is 18 bytes, verify that there are exactly 8 bytes in the buffer
 
-    // set channel values
-    c0 = ((m_inputRaw[1] & 0x7) << 8) | m_inputRaw[0];
-    c1 = ((m_inputRaw[2] & 0x3f) << 5) | ((m_inputRaw[1] & 0xf8) >> 3);
-    c2 = ((m_inputRaw[4] & 0x01) << 10) | ((m_inputRaw[3] & 0xff) << 2) | ((m_inputRaw[2] & 0xc0) >> 6);
-    c3 = ((m_inputRaw[5] & 0x0f) << 7) | ((m_inputRaw[4] & 0xfe) >> 1);
-    s1 = (m_inputRaw[5] & 0x30) >> 4;
-    s2 = (m_inputRaw[5] & 0xc0) >> 6;
-    wheel = ((m_inputRaw[17] & 0x7) << 8) | m_inputRaw[16];
+	// clear if there are more than 18 bytes, i.e. we missed a previous packet
+	if (Serial5.available() > DR16_PACKET_SIZE)
+	{
+		Serial5.clear();
+		return;
+	}
 
-    if (IsDataValid())
-    {
-        // joy sticks
-        m_input[0] = bounded_map(c0, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
-        m_input[1] = bounded_map(c1, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
-        m_input[2] = bounded_map(c2, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
-        m_input[3] = bounded_map(c3, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
+	// dont read if there are less than 18 bytes, i.e. we caught the packet as it was being written
+	if (Serial5.available() < DR16_PACKET_SIZE)
+	{
+		return;
+	}
 
-        // wheel
-        m_input[4] = bounded_map(wheel, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
+	// issue read command, fills m_inputRaw with 18 bytes
+	Serial5.readBytes(m_inputRaw, DR16_PACKET_SIZE);
 
-        // switches
-        m_input[5] = (float)s1;
-        m_input[6] = (float)s2;
-    }
+	// set channel values, since each channel is packed within each other, and are 11 bits long
+	// some bit shifting is required
+	c0 = ((m_inputRaw[1] & 0x07) << 8) 	| m_inputRaw[0];
+	c1 = ((m_inputRaw[2] & 0x3f) << 5) 	| ((m_inputRaw[1] & 0xf8) >> 3);
+	c2 = ((m_inputRaw[4] & 0x01) << 10)	| ((m_inputRaw[3] & 0xff) << 2) | ((m_inputRaw[2] & 0xc0) >> 6);
+	c3 = ((m_inputRaw[5] & 0x0f) << 7) 	| ((m_inputRaw[4] & 0xfe) >> 1);
+	wh = ((m_inputRaw[17] & 0x7) << 8) 	| m_inputRaw[16];
+	s1 = (m_inputRaw[5] & 0x30) >> 4;
+	s2 = (m_inputRaw[5] & 0xc0) >> 6;
 
-    // Serial.printf("0: %d\t1: %d\t2: %d\t3: %d\ts1: %d\ts2: %d\twheel: %d\n", c0, c1, c2, c3, s1, s2, wheel);
+	// assign formated data (within ranges of [-1,1]) to the true input buffer
+	// joy sticks
+	m_input[0] = bounded_map(c0, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
+	m_input[1] = bounded_map(c1, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
+	m_input[2] = bounded_map(c2, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
+	m_input[3] = bounded_map(c3, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
 
-    // for (int i = 0; i < DR16_PACKET_SIZE; i++)
-    //     Serial.printf("%x\t", m_inputRaw[i]);
-    // Serial.println();
-    Print();
+	// wheel
+	m_input[4] = bounded_map(wh, DR16_CONTROLLER_INPUT_LOW, DR16_CONTROLLER_INPUT_HIGH, -1000, 1000) / 1000.f;
+	
+	// switches
+	m_input[5] = (float)s1;
+	m_input[6] = (float)s2;
+
+	Print();
 }
 
 float* DR16::GetInput()
 {
-    return nullptr;
+	return m_input;
 }
 
 void DR16::Print()
 {
-    Serial.printf("%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n", m_input[0], m_input[1], m_input[2], m_input[3], m_input[4], m_input[5], m_input[6]);
+	Serial.printf("%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n", m_input[0], m_input[1], m_input[2], m_input[3], m_input[4], m_input[5], m_input[6]);
+}
+
+void DR16::PrintRaw()
+{
+	for (int i = 0; i < DR16_PACKET_SIZE; i++)
+		Serial.printf("%.2x\t", m_inputRaw[i]);
+	Serial.println();
 }
 
 float DR16::bounded_map(int value, int in_low, int in_high, int out_low, int out_high)
 {
-    /*
-        This is derived from sthe arduino map() function.
-    */
-    value = max(min(value, in_high), in_low);
-    return (value - in_low) * (out_high - out_low) / (in_high - in_low) + out_low;
+	// this is derived from sthe arduino map() function
+	value = max(min(value, in_high), in_low);
+	
+	return (value - in_low) * (out_high - out_low) / (in_high - in_low) + out_low;
 }
--- DR16/DR16.hpp
+++ DR16/DR16.hpp
@@ -1,22 +1,22 @@
 #ifndef DR16_HPP
 #define DR16_HPP
 
-#include <cstdint>
-#include "Arduino.h"
+#include <cstdint>		// for access to fixed-width types
+#include "Arduino.h"	// for access to HardwareSerial defines
 
-constexpr uint16_t DR16_PACKET_SIZE = 18;
-constexpr uint16_t DR16_INPUT_VALUE_COUNT = 7;
+constexpr uint16_t DR16_PACKET_SIZE 			= 18;	// the size in bytes of a DR16-Receiver packet
+constexpr uint16_t DR16_INPUT_VALUE_COUNT 		= 7;	// the size in floats of the normalized input
 
-constexpr uint16_t DR16_CONTROLLER_INPUT_HIGH = 1684;
-constexpr uint16_t DR16_CONTROLLER_INPUT_LOW = 364;
-constexpr uint16_t DR16_CONTROLLER_INPUT_ZERO = 1024;
+constexpr uint16_t DR16_CONTROLLER_INPUT_HIGH 	= 1684;	// the maximum joystick input value
+constexpr uint16_t DR16_CONTROLLER_INPUT_LOW 	= 364;	// the minimum joystick input value
+constexpr uint16_t DR16_CONTROLLER_INPUT_ZERO 	= 1024;	// the medium joystick input value
 
-constexpr uint16_t DR16_CONTROLLER_SWITCH_HIGH 	= 3;
-constexpr uint16_t DR16_CONTROLLER_SWITCH_LOW 	= 1;
+constexpr uint16_t DR16_CONTROLLER_SWITCH_HIGH 	= 3;	// the maximum switch input value
+constexpr uint16_t DR16_CONTROLLER_SWITCH_LOW 	= 1;	// the minimum switch input value
 
 /// DR16 Packet Structure
 /// (translated from this: https://rm-static.djicdn.com/tem/17348/4.RoboMaster%20%E6%9C%BA%E5%99%A8%E4%BA%BA%E4%B8%93%E7%94%A8%E9%81%A5%E6%8E%A7%E5%99%A8%EF%BC%88%E6%8E%A5%E6%94%B6%E6%9C%BA%EF%BC%89%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C.pdf)
-// 
+
 // Total Size: 18 bytes
 // BYTE | BIT layout (format is 'channel # . bit # of channel')
 // 1    |	c0.7	c0.6	c0.5	c0.4	c0.3	c0.2	c0.1	c0.0
@@ -35,14 +35,14 @@ constexpr uint16_t DR16_CONTROLLER_SWITCH_LOW 	= 1;
 // 14   |							m2	(0-7)
 // 15   |						KEY 		(0-7)
 // 16   |						KEY 		(8-15)
-// 17   |							wheel
-// 18   |							RESERVED
-//
+// 17   |						RESERVED (wheel [0-7])
+// 18   |						RESERVED (wheel [5-7])
+
 // Channel 	|	Physical Action			|	Value Ranges [low - '0' - high]
 // c0		|	Right Joystick X-Axis	|	[364 - 1024 - 1684]
 // c1		| 	Right Joystick Y-Axis	| 	[364 - 1024 - 1684]
-// c2 		| 	Left Joystick X-Axis	|	[364 - 1024 - 1684]
-// c3 		| 	Left Joystick Y-Axis	|	[364 - 1024 - 1684]
+// c2		| 	Left Joystick X-Axis	|	[364 - 1024 - 1684]
+// c3		| 	Left Joystick Y-Axis	|	[364 - 1024 - 1684]
 // s1		| 	Left Switch				| 	[1 - 3]
 // s2		| 	Right Switch			| 	[1 - 3]
 // mX		| 	Mouse X-Axis			|	[-32768 - 0 - 32767]
@@ -51,31 +51,51 @@ constexpr uint16_t DR16_CONTROLLER_SWITCH_LOW 	= 1;
 // m1		|	Mouse Left Button		| 	[0 - 1]
 // m2		|	Mouse Right Button		| 	[0 - 1]
 // key		|	Specific Key Presses	|	Bitmap: (bit #) (0 - 1)
-//												W: 0	Q: 		4
-//												S: 1	E: 		5
-//												A: 2	Shift: 	6
-//												D: 3	Ctrl: 	7
+//												W: 0	Q:		4
+//												S: 1	E:		5
+//												A: 2	Shift:	6
+//												D: 3	Ctrl:	7
+// wheel	|	Wheel Axis				|	[364 - 1024 - 1684]
 
-
+/// @brief Wrapper for reading and mapping input from the DR16-Receiver and associated controller
 class DR16
 {
 public:
+	/// @brief Constructor, left empty
 	DR16();
 
+	/// @brief Initializes DR16 receiver, starts the Serial interface, and zeros input buffers
 	void Init();
 
+	/// @brief Attempts to read a full packet from the receiver. This function shouldn't be ran more than 100kHz
 	void Read();
 
+public:
+	/// @brief Get the 7 float length input buffer. These values are normalized [-1, 1]
+	/// @return float buffer
 	float* GetInput();
 
+	/// @brief Prints the normalized input buffer
 	void Print();
 
+	/// @brief Prints the raw 18-byte packet from the receiver
+	void PrintRaw();
+
 private:
+	/// @brief Maps the input value to a specified value range
+	/// @param value the input value
+	/// @param in_low the lowest value the input could be
+	/// @param in_high the highest value the input could be
+	/// @param out_low the lowest the mapped value should be
+	/// @param out_high the highest the mapped value should be
+	/// @return Mapped input in the range of [out_low, out_high]
 	float bounded_map(int value, int in_low, int in_high, int out_low, int out_high);
-	
+
 private:
+	/// @brief normalized input buffer
 	float m_input[DR16_INPUT_VALUE_COUNT] = { 0 };
 
+	/// @brief non-normalized, raw 18 byte packet
 	uint8_t m_inputRaw[DR16_PACKET_SIZE] = { 0 };
 
 };
