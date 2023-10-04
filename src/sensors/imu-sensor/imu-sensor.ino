401da76100e99755db158a1d3627a04c66285d8f
StudentKyle <studentkyles@gmail.com>
2023-10-03 20:29:06 -0600

    Begins running code

--- src/sensors/imu-sensor/imu-sensor.ino
+++ src/sensors/imu-sensor/imu-sensor.ino
@@ -1,7 +1,44 @@
-void setup() {
+#include "ICM20649.hpp"
+#include "LSM6DSOX.hpp"
 
+ICM20649 icm;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit ICM20649 test!");
+
+  // Try to initialize!
+
+
+  Serial.println("ICM20649 Found!");
 }
 
 void loop() {
-    
+
+  Serial.print("\t\tTemperature ");
+  Serial.print(icm.get_temperature());
+  Serial.println(" deg C");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(icm.get_accel_X());
+  Serial.print(" \tY: ");
+  Serial.print(icm.get_accel_Y());
+  Serial.print(" \tZ: ");
+  Serial.print(icm.get_accel_Z());
+  Serial.println(" m/s^2 ");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tGyro X: ");
+  Serial.print(icm.get_gyro_X());
+  Serial.print(" \tY: ");
+  Serial.print(icm.get_gyro_X());
+  Serial.print(" \tZ: ");
+  Serial.print(icm.get_gyro_Z());
+  Serial.println(" radians/s ");
+  Serial.println();
+  delay(10);
 }
