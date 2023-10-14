5d77e150ba58941f54e9beb13d147eddcc792deb
Pandabear1125 <jvsdestroyer25@gmail.com>
2023-10-14 16:56:31 -0600

    Preparing for merge.

---
+++ src/sensors/RefSystem.hpp
@@ -0,0 +1,37 @@
+#ifndef REF_SYSTEM_HPP
+#define REF_SYSTEM_HPP
+
+#include <cassert>
+#include "Arduino.h"
+#include <set>
+
+#include "RefSystemPacketDefs.hpp"
+
+
+class RefSystem
+{
+public:
+    RefSystem();
+
+    void Init();
+
+    bool Read();
+
+    bool Process();
+
+public:
+
+
+public:
+    bool ReadFrameStart(Frame& frame);
+    bool ReadFrameCommandID(Frame& frame);
+    bool ReadFrameData(Frame& frame);
+    bool ReadFrameEnd(Frame& frame);
+
+private:
+    uint8_t m_rawData[REF_MAX_FRAME_DATA_SIZE] = { 0 };
+    uint8_t m_frameDataSection[REF_MAX_FRAME_DATA_SIZE] = { 0 };
+
+};
+
+#endif // REF_SYSTEM_HPP
