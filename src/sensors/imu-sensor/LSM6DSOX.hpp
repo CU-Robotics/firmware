344545ac2031db161836155444f62148cdfed78b
StudentKyle <studentkyles@gmail.com>
2023-10-31 14:13:31 -0600

    Adds a note about I2C and SPI support on the specific LSM6DSOX we use

--- src/sensors/imu-sensor/LSM6DSOX.hpp
+++ src/sensors/imu-sensor/LSM6DSOX.hpp
@@ -6,6 +6,7 @@
 #include <Adafruit_LSM6DSOX.h>
 
 /// @brief Sensor access for an LSM6DSOX IMU Sensor. Child of the abstract IMUSensor class. 
+/// @note We currently use the LSM6DSOX+LIS3MDL, which has an accelerometer, gyroscope, and magnetometer. This board only supports I2C (not SPI) communication.
 class LSM6DSOX : public IMUSensor {
 public:
     /// @brief Constructor. Currently does nothing, use init() instead.
