bcc1bfd72eae55d753037c80464fc1f9601780e6
Aidan Donnelly <73967906+AidanDonnelly1@users.noreply.github.com>
2023-10-31 20:04:47 -0600

    fixed bug with averaging readings

--- src/sensors/rev_encoder.cpp
+++ src/sensors/rev_encoder.cpp
@@ -1,45 +1,45 @@
 /**
  * @file rev_encoder.cpp
- * @brief The implementation of the Rev Encoder firmware 
- * @date 2023-10-14
+ * @brief The implementation of the Rev Encoder 
+ * @date 2023-10-31
  */
-
 #include <rev_encoder.hpp>
 
-RevEncoder::RevEncoder(uint8_t encPin, int baudrate)
+RevEncoder::RevEncoder(uint8_t encPin, int baudrate) 
 {
-    this->inPin = encPin;
-    pinMode(this->inPin, INPUT); // Set the pin used to measure the encoder to be an input
-    Serial.begin(baudrate);
-    freq.begin(this->inPin, FREQMEASUREMULTI_MARK_ONLY);
+  this->inPin = encPin;
+  this->sum = 0;
+  this->count = 0;
+  pinMode(this->inPin, INPUT);  // Set the pin used to measure the encoder to be an input
+  Serial.begin(baudrate);
+  freq.begin(this->inPin, FREQMEASUREMULTI_MARK_ONLY);
+  
 }
 
 void RevEncoder::read() 
 {
-    float sum = 0;
-    int count = 0;  
-    if (freq.available()) 
+  if (freq.available()) 
+  {
+    // average several reading together
+    this->sum += this->freq.read();
+    this->count++;
+    if (this->count >= this->READ_SIZE)  //averges the angle across n readings
     {
-        // average several reading together
-        sum += freq.read();
-        count++;
-        if (count >= this->READ_SIZE) //averges the angle across READ_SIZE readings
-        {
-            float frequency = freq.countToNanoseconds(sum / count) / 1000;
-            this->ticks = (int) frequency % 1024;
-            this->radians = (this->ticks/1024) * M_PI * 2;
-            sum = 0;
-            count = 0;
-        }
+      float frequency = freq.countToNanoseconds(sum / count) / 1000;
+      this->ticks = (int) frequency % 1024;
+      this->radians = (this->ticks / 1024) * M_PI * 2;
+      this->sum = 0;
+      this->count = 0;
     }
+  }
 }
 
-float RevEncoder::getAngleTicks()
+float RevEncoder::getAngleTicks() 
 {
-    return this->ticks;
+  return this->ticks;
 }
 
-float RevEncoder::getAngleRadians()
+float RevEncoder::getAngleRadians() 
 {
-    return this->radians;
+  return this->radians;
 }
--- src/sensors/rev_encoder.hpp
+++ src/sensors/rev_encoder.hpp
@@ -10,59 +10,56 @@
 #ifndef REV_ENCODER_H
 #define REV_ENCODER_H
 
-/**
- * @brief Rev Encoder Class
- */
 class RevEncoder 
 {
 	private:
 		/**
-		 * @brief the pin number that the encoders signal pin is plugged into
-		 */
+		* @brief the pin number that the encoders signal pin is plugged into
+		*/
 		uint8_t inPin;
 		/**
-		 * @brief The baudrate for the encoder
-		 */
-		int baudrate;
-		/**
-		 * @brief Used to read rise time of the encoder
-		 */
+		* @brief Used to read rise time of the encoder
+		*/
 		FreqMeasureMulti freq;
 		/**
 		 * @brief how many readings will be avergaed to update the current angle 
 		 */
 		const int READ_SIZE = 10;
 		/**
-		 * @brief measure of current angle in ticks (1024 ticks = 1 rotation )
+		 * @brief used to add up sequencial encoder readings to evenutually average
 		 */
+		float sum;
+		/** 
+		 * @brief to track how many encoder readings are used to eventually average 
+		 */
+		int count;
+		/**
+		* @brief measure of current angle in ticks (1024 ticks = 1 rotation). When set to 1 individual readings are returned
+		*/
 		float ticks;
 		/**
-		 * @brief measure of current angle in radians 
-		 */
+		* @brief measure of current angle in radians 
+		*/
 		float radians;
 	public:
 		/**
-		 * @brief Construct a new rev_encoder object
-		 * 
-		 * @param encPin the pin number that the encoders signal pin is plugged into
-		 * @param baudrate the desired baudrate for the encoder
-		 */
+		* @brief Construct a new rev_encoder object
+		* @param encPin the pin number that the encoders signal pin is plugged into
+		* @param baudrate the desired baudrate for the encoder
+		*/
 		RevEncoder(uint8_t encPin, int baudrate);
 		/**
-		 * @brief updates the current angle in ticks (1024 = 1 Rotation) and radians.
-		 */
-        void read();
+		* @brief updates the current angle in ticks (1024 = 1 Rotation) and radians.
+		*/
+		void read();
 		/**
-		 * @brief get the last angle of the encoder in ticks
-		 * @return the last angle of the encoder in ticks
-		 */
+		* @brief return the last angle of the encoder in ticks (1024 = 1 Rotation).
+		*/
 		float getAngleTicks();
 		/**
-		 * @brief get the last angle of the encoder in radians
-		 * @return the last angle of the encoder in radians
-		 */
+		* @brief return the last angle of the encoder in radians
+		*/
 		float getAngleRadians();
-		
 };
 
 #endif
