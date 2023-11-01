778475f82b4e29605207eb82fa79e1080f374afa
Aidan Donnelly <73967906+AidanDonnelly1@users.noreply.github.com>
2023-10-31 20:45:24 -0600

    forgot to delete sum and count

--- src/sensors/rev_encoder.cpp
+++ src/sensors/rev_encoder.cpp
@@ -8,23 +8,18 @@
 RevEncoder::RevEncoder(uint8_t encPin, int baudrate) 
 {
   this->inPin = encPin;
-  this->sum = 0;
-  this->count = 0;
   pinMode(this->inPin, INPUT);  // Set the pin used to measure the encoder to be an input
   Serial.begin(baudrate);
   freq.begin(this->inPin, FREQMEASUREMULTI_MARK_ONLY);
-  
 }
 
 void RevEncoder::read() 
 {
   if (freq.available()) 
     {
-      float frequency = freq.countToNanoseconds(this->freq.read()) / 1000;
+      float frequency = this->freq.countToNanoseconds(this->freq.read()) / 1000;
       this->ticks = (int) frequency % 1024;
       this->radians = (this->ticks / 1024) * M_PI * 2;
-      this->sum = 0;
-      this->count = 0;
     }
 }
 
