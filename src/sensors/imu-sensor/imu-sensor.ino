5dc5e8011d4f1da70db31267aaebac694602edc2
StudentKyle <studentkyles@gmail.com>
2023-10-31 19:48:28 -0600

    Tests the LSM, it works. TODO test the ICM

--- src/sensors/imu-sensor/imu-sensor.ino
+++ src/sensors/imu-sensor/imu-sensor.ino
@@ -2,6 +2,7 @@
 #include "LSM6DSOX.hpp"
 
 ICM20649 icm;
+LSM6DSOX lsm;
 
 void setup() {
   Serial.begin(115200);
@@ -11,35 +12,63 @@ void setup() {
   Serial.println("Adafruit ICM20649 test!");
 
   // Try to initialize!
-  ICM20649::CommunicationProtocol protocol = ICM20649::CommunicationProtocol::SPI;
-  icm.init(protocol);
+
+  // ICM
+  // ICM20649::CommunicationProtocol protocol = ICM20649::CommunicationProtocol::SPI;
+  // icm.init(protocol);
+
+  // LSM
+  lsm.init();
 
   Serial.println("ICM20649 Found!");
 }
 
 void loop() {
-  icm.read();
-  Serial.print("\t\tTemperature ");
-  Serial.print(icm.get_temperature());
-  Serial.println(" deg C");
+  // icm.read();
+  // Serial.print("\t\tTemperature ");
+  // Serial.print(icm.get_temperature());
+  // Serial.println(" deg C");
 
+  // /* Display the results (acceleration is measured in m/s^2) */
+  // Serial.print("\t\tAccel X: ");
+  // Serial.print(icm.get_accel_X());
+  // Serial.print(" \tY: ");
+  // Serial.print(icm.get_accel_Y());
+  // Serial.print(" \tZ: ");
+  // Serial.print(icm.get_accel_Z());
+  // Serial.println(" m/s^2 ");
+
+  // /* Display the results (acceleration is measured in m/s^2) */
+  // Serial.print("\t\tGyro X: ");
+  // Serial.print(icm.get_gyro_X());
+  // Serial.print(" \tY: ");
+  // Serial.print(icm.get_gyro_X());
+  // Serial.print(" \tZ: ");
+  // Serial.print(icm.get_gyro_Z());
+  // Serial.println(" radians/s ");
+  // Serial.println();
+  // delay(10);
+
+  lsm.read();
+  Serial.print("\t\tTemperature ");
+  Serial.print(lsm.get_temperature());
+  Serial.println(" deg C");
   /* Display the results (acceleration is measured in m/s^2) */
   Serial.print("\t\tAccel X: ");
-  Serial.print(icm.get_accel_X());
+  Serial.print(lsm.get_accel_X());
   Serial.print(" \tY: ");
-  Serial.print(icm.get_accel_Y());
+  Serial.print(lsm.get_accel_Y());
   Serial.print(" \tZ: ");
-  Serial.print(icm.get_accel_Z());
+  Serial.print(lsm.get_accel_Z());
   Serial.println(" m/s^2 ");
-
   /* Display the results (acceleration is measured in m/s^2) */
   Serial.print("\t\tGyro X: ");
-  Serial.print(icm.get_gyro_X());
+  Serial.print(lsm.get_gyro_X());
   Serial.print(" \tY: ");
-  Serial.print(icm.get_gyro_X());
+  Serial.print(lsm.get_gyro_X());
   Serial.print(" \tZ: ");
-  Serial.print(icm.get_gyro_Z());
+  Serial.print(lsm.get_gyro_Z());
   Serial.println(" radians/s ");
   Serial.println();
-  delay(10);
+  delay(100);
 }
