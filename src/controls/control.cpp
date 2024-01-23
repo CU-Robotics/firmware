9541c943e90b9bc99c30486199ae2992b80eca66
Isaac Wilcove <isaac@wilcove.com>
2024-01-23 16:40:05 -0700

    Small changes

--- src/controls/control.cpp
+++ src/controls/control.cpp
@@ -1,9 +1,5 @@
 #include "control.hpp"
 
-float powerlimit_ratio (){//return powerlimit_ratio (Because we might use the number at the same time, better not call it in each step)   
-    float power_buffer = ref.data.power_heat.buffer_energy;
-    return constrain(power_buffer / 20, 0.0, 1.0);
-} 
 void Control::init_controller(uint8_t can_id, uint8_t motor_id, int controller_type) {
     int index = ((can_id-1)*NUM_MOTORS_PER_BUS) + (motor_id-1);
 
