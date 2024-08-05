c5e40f924c7a8050aa452361c11098376ee607b0
StudentKyle <studentkyles@gmail.com>
2024-08-05 17:43:15 -0600

    fix sudo permissions in script

--- tools/install_tytools.sh
+++ tools/install_tytools.sh
@@ -1,14 +1,14 @@
 # makes the keyrings directory with correct permission
-mkdir -p -m0755 /etc/apt/keyrings
+sudo mkdir -p -m0755 /etc/apt/keyrings
 
 # downloads the key for the repo for apt to install from
 curl https://download.koromix.dev/debian/koromix-archive-keyring.gpg -o /etc/apt/keyrings/koromix-archive-keyring.gpg
 
 # adds the repo to the apt list
-echo "deb [signed-by=/etc/apt/keyrings/koromix-archive-keyring.gpg] https://download.koromix.dev/debian stable main" > /etc/apt/sources.list.d/koromix.dev-stable.list
+echo "deb [signed-by=/etc/apt/keyrings/koromix-archive-keyring.gpg] https://download.koromix.dev/debian stable main" | sudo tee /etc/apt/sources.list.d/koromix.dev-stable.list
 
 # updates apt
-apt update
+sudo apt update
 
 # install tytools
-apt install -y tytools
+sudo apt install -y tytools
