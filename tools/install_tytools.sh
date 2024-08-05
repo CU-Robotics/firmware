43b124d11b744156261b8ab73bfcc8e108cfc4e1
StudentKyle <studentkyles@gmail.com>
2024-08-05 17:37:37 -0600

    improve comments

--- tools/install_tytools.sh
+++ tools/install_tytools.sh
@@ -1,10 +1,10 @@
-# adds something (required idk)
+# makes the keyrings directory with correct permission
 mkdir -p -m0755 /etc/apt/keyrings
 
-# downloads the repo for apt to install from
+# downloads the key for the repo for apt to install from
 curl https://download.koromix.dev/debian/koromix-archive-keyring.gpg -o /etc/apt/keyrings/koromix-archive-keyring.gpg
 
-# adds the repo for apt to install from
+# adds the repo to the apt list
 echo "deb [signed-by=/etc/apt/keyrings/koromix-archive-keyring.gpg] https://download.koromix.dev/debian stable main" > /etc/apt/sources.list.d/koromix.dev-stable.list
 
 # updates apt
