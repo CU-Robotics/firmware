1932df4545ecef49879ffd5390693926d6f06ac6
Rocco Antoniou <roccoantoniou@gmail.com>
2024-09-10 15:46:29 -0600

    Fixed multiple sections at the same nesting level

--- src/utils/profiler.hpp
+++ src/utils/profiler.hpp
@@ -1,56 +1,60 @@
 #ifndef PROFILER_H
 #define PROFILER_H
 
-// IMPORTANT! Use this flag to toggle profiling globally.
-// The compiler should optimize out the functions with empty bodies.
-// #define PROFILE
-
-// TODO: make 2:1 nested sections work and finisht the PROFILE flag
-// TODO: profile main
+// Use this flag to toggle profiling globally.
+#define PROFILE
 
 #include <Arduino.h>
 
-#define PROF_MAX_SECTIONS 4 // max nested profiling sections
-#define PROF_MAX_NAME 8 // max length of section name
-#define PROF_MAX_TIMES 256 // max number of start/end times per profiler
+#define PROF_MAX_SECTIONS 4  // max number of active profiling sections
+#define PROF_MAX_NAME 16      // max length of section name
+#define PROF_MAX_TIMES 256   // max number of start/end times per section
 
 /// @brief Object for profiling sections of code.
-/// Limitation: Nesting two profiler sections at the same level doesn't work properly, i.e. main { test1 {} test2 {} } will not work as expected
 struct Profiler {
     struct profiler_section_t {
         uint32_t start_times[PROF_MAX_TIMES];
         uint32_t end_times[PROF_MAX_TIMES];
         uint8_t count = 0;
-        char name[PROF_MAX_NAME + 1]; // extra for null terminator
+        char name[PROF_MAX_NAME + 1];  // extra for null terminator
     };
 
-    profiler_section_t stack[PROF_MAX_SECTIONS];
-    int8_t top = -1; // top index, -1 means empty
+    profiler_section_t sections[PROF_MAX_SECTIONS];
 
-    /// @brief Start (push) a profiling section onto the stack.
+    /// @brief Start a profiling section.
     /// @param name A unique name to identify the section.
     void begin(const char* name) {
     #ifdef PROFILE
-        if (top >= PROF_MAX_SECTIONS - 1) return;
-        top++;
-        strncpy(stack[top].name, name, PROF_MAX_NAME);
-        stack[top].name[PROF_MAX_NAME] = '\0'; // ensure null termination
-        if (stack[top].count < PROF_MAX_TIMES) {
-            stack[top].start_times[stack[top].count] = micros();
+        // search for a section by name or an empty slot
+        for (uint8_t i = 0; i < PROF_MAX_SECTIONS; i++) {
+            if (strncmp(sections[i].name, name, PROF_MAX_NAME) == 0 || sections[i].count == 0) {
+                // start this section
+                strncpy(sections[i].name, name, PROF_MAX_NAME);
+                sections[i].name[PROF_MAX_NAME] = '\0';  // ensure null termination
+                if (sections[i].count < PROF_MAX_TIMES) {
+                    sections[i].start_times[sections[i].count] = micros();
+                }
+                return;
+            }
         }
     #endif
     }
 
-    /// @brief End (pop) the newest profiling section. Don't call without a corresponding push.
-    void end() {
+    /// @brief End a profiling section.
+    /// @param name The name of the section to end.
+    void end(const char* name) {
     #ifdef PROFILE
-        if (top < 0) return;
-        uint8_t count = stack[top].count;
-        if (count < PROF_MAX_TIMES) {
-            stack[top].end_times[count] = micros();
-            stack[top].count++;
+        // find the section by name and add an end time
+        for (uint8_t i = 0; i < PROF_MAX_SECTIONS; i++) {
+            if (strncmp(sections[i].name, name, PROF_MAX_NAME) == 0) {
+                uint8_t count = sections[i].count;
+                if (count < PROF_MAX_TIMES) {
+                    sections[i].end_times[count] = micros();
+                    sections[i].count++;
+                }
+                return;
+            }
         }
-        top--;
     #endif
     }
 
@@ -63,17 +67,17 @@ struct Profiler {
         uint32_t sum = 0;
 
         for (uint8_t i = 0; i < PROF_MAX_SECTIONS; i++) {
-            if (strncmp(stack[i].name, name, PROF_MAX_NAME) != 0) continue;
-            for (uint8_t j = 0; j < stack[i].count; j++) {
-                uint32_t delta = stack[i].end_times[j] - stack[i].start_times[j];
+            if (strncmp(sections[i].name, name, PROF_MAX_NAME) != 0) continue;
+            for (uint8_t j = 0; j < sections[i].count; j++) {
+                uint32_t delta = sections[i].end_times[j] - sections[i].start_times[j];
                 sum += delta;
                 if (delta < min) min = delta;
                 if (delta > max) max = delta;
             }
-            // Print stats
-            if (stack[i].count > 0) {
+            // print stats
+            if (sections[i].count > 0) {
                 Serial.printf("Profiling for: %s\n  Min: %d us\n  Max: %d us\n  Avg: %d us\n",
-                                name, min, max, sum / stack[i].count);
+                                name, min, max, sum / sections[i].count);
             }
             return;
         }
@@ -81,6 +85,6 @@ struct Profiler {
     }
 };
 
-extern Profiler prof; // Global profiler
+extern Profiler prof;  // Global profiler
 
-#endif // PROFILER_H
+#endif  // PROFILER_H
