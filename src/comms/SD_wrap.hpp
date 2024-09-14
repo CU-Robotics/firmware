60064808a0041e078261034f9f5513fb196534d1
ashleycody345 <ashley.cody@colorado.edu>
2024-09-14 17:23:23 -0600

    prototype SD wrapper class

--- src/comms/SD_wrap.hpp
+++ src/comms/SD_wrap.hpp
@@ -1,10 +1,37 @@
 #ifndef SD_WRAP
 #define SD_WRAP
 
-#include <Arduino.h>
-#include <SD.h>
-#include <SPI.h>
+#include "../../libraries/SD/SD.h"
+#include "../../libraries/SPI/SPI.h"
 
+#define _DIR_LENGTH 128
 
+class SD {
+    public:
+
+    SD();
+
+    int open(char* filepath);
+    void close();
+
+    int mkfile(char* filename);
+    int mkdir(char* dirname);
+    int rm(char* filename, bool r);
+    int rm(char* filename);
+
+    int read(uint8_t* dest, unsigned int len);
+    int write(uint8_t* src, unsigned int len);
+    
+    bool exists(char* filepath);
+    void enumerate_files(char* root, bool r);
+
+    private:
+
+    void enumerate_files(char* root, bool r, int tabs);
+
+    SDClass SDinternal;
+    
+    File file;
+};
 
 #endif
