9ed86ce4956093273b470148d3a347c2f0d9f01e
JimOAOOO <yili4413@colorado.edu>
2024-09-16 09:22:18 -0600

    SELF_CHECK

--- src/Balancing-Matlab_trans/Balancing_Control.cpp
+++ src/Balancing-Matlab_trans/Balancing_Control.cpp
@@ -8,39 +8,52 @@ void BalancingControl::init(){
     pid1.set_K(gain1);
     pid2.set_K(gain2);
     // p[rows = P_LOCO_ROW] [cols = XHELP_LENGTH]
-    float tempp[6][P_LOCO_ROW][10] =
-        {{{1,0,0,0,0,0,0,0,0,0},  
-        {0,2,0,0,0,0,0,0,0,0}, 
-        {0,0,3,0,0,0,0,0,0,0}, 
-        {0,0,0,4,0,0,0,0,0,0}},
-        {{1,0,0,0,0,0,0,0,0,0},  
-        {0,2,0,0,0,0,0,0,0,0}, 
-        {0,0,3,0,0,0,0,0,0,0}, 
-        {0,0,0,4,0,0,0,0,0,0}},
-        {{1,0,0,0,0,0,0,0,0,0},  
-        {0,2,0,0,0,0,0,0,0,0}, 
-        {0,0,3,0,0,0,0,0,0,0}, 
-        {0,0,0,4,0,0,0,0,0,0}},
-        {{1,0,0,0,0,0,0,0,0,0},  
-        {0,2,0,0,0,0,0,0,0,0}, 
-        {0,0,3,0,0,0,0,0,0,0}, 
-        {0,0,0,4,0,0,0,0,0,0}},
-        {{1,0,0,0,0,0,0,0,0,0},  
-        {0,2,0,0,0,0,0,0,0,0}, 
-        {0,0,3,0,0,0,0,0,0,0}, 
-        {0,0,0,4,0,0,0,0,0,0}},
-        {{1,0,0,0,0,0,0,0,0,0},  
-        {0,2,0,0,0,0,0,0,0,0}, 
-        {0,0,3,0,0,0,0,0,0,0}, 
-        {0,0,0,4,0,0,0,0,0,0}}};
+    float tempp[6][P_LOCO_ROW][10] = {{
+        {0,0,0,0,0,0,0,0,0,0},  
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}},
+        {
+        {0,0,0,0,0,0,0,0,0,0},  
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}},
+        {
+        {0,0,0,0,0,0,0,0,0,0},  
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}},
+        {
+        {0,0,0,0,0,0,0,0,0,0},  
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}},
+        {
+        {0,0,0,0,0,0,0,0,0,0},  
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}},
+        {
+        {0,0,0,0,0,0,0,0,0,0},  
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0}
+        }};
         
     memcpy(p,tempp,sizeof(tempp));
 }
 
 void BalancingControl::step(float output[NUM_MOTORS], float x_d[XHELP_LENGTH], float psi_d, float l_d, float x[XHELP_LENGTH], float psi, float ll, float lr, float jl[2][2], float jr[2][2], float a_z, float ll_ddot, float lr_ddot){
+
     float dt = timer.delta(); 
     float l = (ll + lr) / 2; 
-
+    /** Initialize all output variables */
+    output[T_LWL_OUTPUT_NUM] = 0;
+    output[T_JLF_OUTPUT_NUM] = 0;
+    output[T_JLB_OUTPUT_NUM] = 0;
+    output[T_LWR_OUTPUT_NUM] = 0;
+    output[T_JRF_OUTPUT_NUM] = 0;
+    output[T_JRB_OUTPUT_NUM] = 0;
     /** This is the part for leg_controller */
     float F_psi = pid1.filter(dt, BOUND, WARP) * (psi_d - psi); 
     float F_l = pid2.filter(dt, BOUND, WARP) * (l_d - l); 
@@ -70,15 +83,8 @@ void BalancingControl::step(float output[NUM_MOTORS], float x_d[XHELP_LENGTH], f
     /**The NormalF Right */ //ASK
     float F_whr = F_blr * costheta_r + m_l * (G_CONSTANT + a_z - (1-eta_l) * lr_ddot * costheta_r);
 
-    if(F_whl < F_WH_OUTPUT_LIMIT_NUM && F_whr < F_WH_OUTPUT_LIMIT_NUM){
-        output[T_LWL_OUTPUT_NUM] = 0;
-        output[T_JLF_OUTPUT_NUM] = 0;
-        output[T_JLB_OUTPUT_NUM] = 0;
-        output[T_LWR_OUTPUT_NUM] = 0;
-        output[T_JRF_OUTPUT_NUM] = 0;
-        output[T_JRB_OUTPUT_NUM] = 0;
+    if(F_whl < F_WH_OUTPUT_LIMIT_NUM && F_whr < F_WH_OUTPUT_LIMIT_NUM)
         return;
-    }
 
     /** This is the part for locomotion_controller */
     /** In Acceleration Saturation */
@@ -86,7 +92,7 @@ void BalancingControl::step(float output[NUM_MOTORS], float x_d[XHELP_LENGTH], f
     float dx[XHELP_LENGTH];
     for(int i = 0; i < 10; i++)
         dx[i] = x_d[i] - x[i];
-    if(dx[XHELP_s]>1) dx[XHELP_s] = 1; // constrain is like 2 times slower
+    if(dx[XHELP_s]>1) dx[XHELP_s] = 1;
     else if(dx[XHELP_s] < -1) dx[XHELP_s] = -1;
     /** In Leg Length to K */
     float K[4][10];
@@ -97,7 +103,7 @@ void BalancingControl::step(float output[NUM_MOTORS], float x_d[XHELP_LENGTH], f
     /* for(int i = 0; i < P_LOCO_ROW; i++) //Please change p
         for(int j = 0; j < 10; j++)
             K = p[0][i][j] * l * l * l + p[1][i][j] * l * l + p[2][i][j] * l + p[3][i][j]; */
-    // Fortunatly we get K 4x10
+    // K is 4x10 matrix
     // 4x10 x 10x1 matrix multi (K * dx)
     float T_bll;  
     float T_blr; 
@@ -115,25 +121,25 @@ void BalancingControl::step(float output[NUM_MOTORS], float x_d[XHELP_LENGTH], f
     //2x2 * 2x1 
     // jl[a][b]   *   F_bll
     //   [c][d]       T_bll
-    // = [af + bg]
-    //   [cf + dg]
+    // = [af + bt]
+    //   [cf + dt]
     //Left side check force
     if(F_whl >= F_WH_OUTPUT_LIMIT_NUM){
-        output[T_JLF_OUTPUT_NUM] = jl[0][0] * F_bll + jl[0][1] * F_bll;
+        output[T_JLF_OUTPUT_NUM] = jl[0][0] * F_bll + jl[0][1] * T_bll;
         output[T_JLB_OUTPUT_NUM] = jl[1][0] * F_bll + jl[1][1] * F_bll;
     }else{
         output[T_LWL_OUTPUT_NUM] = 0;
-        output[T_JLF_OUTPUT_NUM] = 0;
-        output[T_JLB_OUTPUT_NUM] = 0;
+        //output[T_JLF_OUTPUT_NUM] = 0;
+        //output[T_JLB_OUTPUT_NUM] = 0; //No need for these lines
     }
     //Right side check force
     if(F_whr >= F_WH_OUTPUT_LIMIT_NUM){
-        output[T_JRF_OUTPUT_NUM] = jr[0][0] * F_blr + jr[0][1] * F_blr;
-        output[T_JRB_OUTPUT_NUM] = jr[1][0] * F_blr + jr[1][1] * F_blr;
+        output[T_JRF_OUTPUT_NUM] = jr[0][0] * F_blr + jr[0][1] * T_blr;
+        output[T_JRB_OUTPUT_NUM] = jr[1][0] * F_blr + jr[1][1] * T_blr;
     }else{
         output[T_LWR_OUTPUT_NUM] = 0;
-        output[T_JRF_OUTPUT_NUM] = 0;
-        output[T_JRB_OUTPUT_NUM] = 0;
+        //output[T_JRF_OUTPUT_NUM] = 0;
+        //output[T_JRB_OUTPUT_NUM] = 0; //No need for these lines
     }
 return;
 }   
