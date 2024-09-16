9e063ec9b416e88a3cd24032fd8f9f7559d4d791
JimOAOOO <yili4413@colorado.edu>
2024-09-16 04:12:43 -0600

    leg_controller_done

--- src/Balancing-Matlab_trans/Balancing_Control.cpp
+++ src/Balancing-Matlab_trans/Balancing_Control.cpp
@@ -8,22 +8,58 @@ void BalancingControl::init(){
 
 }
 
-void BalancingControl::set_refinput(float x_d[10], float psi_d, float l_d){
+void BalancingControl::set_refinput(float x_d[12], float psi_d, float l_d){
 
 }
 
-void BalancingControl::set_fdbinput(float x[10], float psi, float ll, float lr, float jl[4], float jr[4], float a_z){
+void BalancingControl::set_fdbinput(float x[12], float psi, float ll, float lr, float jl[4], float jr[4], float a_z){
 
 }
 
 void BalancingControl::step(float output[NUM_MOTORS]){
     float dt = timer.delta(); 
 
-    // This is the part for leg_controller
-    //l = (_ll + _lr)/2 
-    //_psi_d - _psi need PID
-    //_ld - l need PID
-    //F_psi = pid1.filter(dt, BOUND, WARP);
-    
+    float l = (_ll + _lr) / 2; 
+    /** This is the part for leg_controller */
+        float F_psi = pid1.filter(dt, BOUND, WARP) * (_psi_d - _psi); //Will comment this 
+        float F_l = pid2.filter(dt, BOUND, WARP) * (_l_d - l); // Will comment this
 
-}
+        /** In inertia_ff */
+            //s_dot = _x[XHELP_s_dot], s_ddot = _x[XHELP_s_ddot], phi_dot = _x[XHELP_phi_dot], phi_ddot = _x[XHELP_phi_ddot], theta_ll = _x[XHELP_theta_ll], theta_lr = _x[XHELP_theta_lr]
+            float iffhelp = (m_b / 2 + m_l * eta_l) * (_x[XHELP_phi_ddot] * R_l + _x[XHELP_s_ddot]);
+            float iF_l = -iffhelp * sin(_x[XHELP_theta_ll]);
+            float iF_r = iffhelp * sin(_x[XHELP_theta_lr]); 
+            /* F = (m_b / 2 + eta_l * m_l) * l * _x[XHELP_phi_dot]* _x[XHELP_s_dot] / 2 / R_l;
+            float Fr = F;
+            float Fl = -Fr; */
+
+        /** In gravity_ff */
+            float gffhelp = (m_b / 2 + m_l * THE_C_IDK) * G_CONSTANT;
+            float gF_l = gffhelp * cos(_x[XHELP_theta_ll]);
+            float gF_r = gffhelp * cos(_x[XHELP_theta_lr]);
+
+        float F_bll = F_psi * MA0 + F_l * MA1 + iF_l * MA2 + iF_r * MA3 + gF_l * MA4 + gF_r * MA5; //ASK for if the matrix will change or not
+        float F_blr = F_psi * MA0 + F_l * MA1 + iF_l * MB2 + iF_r * MB3 + gF_l * MB4 + gF_r * MB5;
+
+    /** This is the part for locomotion_controller */
+        /** In Acceleration Saturation */
+            // dx is a array but only used the element 1 in matlab which is 0 here
+            float dx[12];
+            for(int i = 0; i < 12; i++){
+                dx[i] = _x_d[i] - _x[i];
+            }
+            if(dx[XHELP_s] < 1 && dx[XHELP_s] > -1)
+                dx[XHELP_s] = fabs((dx[XHELP_s]) / (dx[XHELP_s]));
+        /** In Leg Length to K */
+            // K = p(:,:,1)*l_l^2 + p(:,:,2)*l_l * l_r + p(:,:,3)*l_l + p(:,:,4)*l_r^2 + p(:,:,5)*l_r+ p(:,:,6);
+
+            // K = p;
+
+            // l = (l_l+l_r)/2;
+            // K = p(:,:,1)*l^3 + p(:,:,2)*l^2 + p(:,:,3)*l + p(:,:,4);
+            
+
+            
+            
+
+}   
--- src/Balancing-Matlab_trans/Balancing_Control.hpp
+++ src/Balancing-Matlab_trans/Balancing_Control.hpp
@@ -4,11 +4,24 @@
 #include "../filters/pid_filter.hpp"
 #include "../utils/timing.hpp"
 
-
-/** IMPORTANT Constants for leg_controller*/
-#define param.m_b 
-#define param.m_l 
-#define param.R_l 
+/** Constants for helping*/
+//for x[12] = [s, s_dot, s_ddot, phi, phi_dot, phi_ddot, theta_ll, theta_ll_dot, theta_lr, theta_lr_dot, theta_b, theta_b_dot]
+#define XHELP_s 0
+#define XHELP_s_dot 1
+#define XHELP_s_ddot 2
+#define XHELP_phi 3
+#define XHELP_phi_dot 4
+#define XHELP_phi_ddot 5
+#define XHELP_theta_ll 6
+#define XHELP_theta_ll_dot 7
+#define XHELP_theta_lr 8
+#define XHELP_theta_lr_dot 9
+#define XHELP_theta_b 10
+#define XHELP_theta_b_dot 11
+/** Constants for leg_controller*/
+#define m_b 
+#define m_l 
+#define R_l 
 #define eta_l
 
 #define K1_P 
@@ -21,21 +34,29 @@
 #define K2_F 
 #define BOUND true
 #define WARP 
-// MatrixMultiply 2x6 constant 
+// MatrixMultiply 2x6 constant (If this doesn't need to change, need to simpfy the calculation) //ASK
 //[a0][a1][a2][a3][a4][a5]
 //[b0][b1][b2][b3][b4][b5]
-#define a0 1
-#define a1 1
-#define a2 1
-#define a3 0
-#define a4 1
-#define a5 0
-#define b0 -1
-#define b1 1
-#define b2 0 
-#define b3 1
-#define b4 0
-#define b5 1
+#define MA0 1
+#define MA1 1
+#define MA2 1
+#define MA3 0
+#define MA4 1
+#define MA5 0
+#define MB0 -1
+#define MB1 1
+#define MB2 0 
+#define MB3 1
+#define MB4 0
+#define MB5 1
+//For gravity_ff
+#define G_CONSTANT 9.81 
+#define THE_C_IDK 0.2868 //ASK
+
+/** Constants for locomotion_controller*/
+#define P_LOCO
+
+
 
 /** Constants for mechanical*/
 #define NUM_MOTORS 6
@@ -44,17 +65,17 @@
 class BalancingControl{
     private:
     /** Variables - Ref */
-        float _x_d[10]; //Checked 
-        float _psi_d; // Checked 
-        float _l_d; // Checked
+        float _x_d[12]; 
+        float _psi_d; 
+        float _l_d; 
     /** Variables - fdb */
-        float _x[10]; // Checked
-        float _psi; // Checked
+        float _x[12]; 
+        float _psi; 
         float _ll; // Left Leg length 
         float _lr; // Right Leg length 
-        float _jl[4]; // Checked
-        float _jr[4]; // Checked 
-        float _a_z; //Checked
+        float _jl[4]; 
+        float _jr[4];
+        float _a_z; 
     /** Helping Classes */
         Timer timer;
         /// @brief The PID for psi
@@ -70,20 +91,20 @@ class BalancingControl{
 
         void init();
         /// @brief ref input(Not sure if those are constant)
-        /// @param x_d = [s, s_dot, phi, phi_dot, theta_ll, theta_ll_dot, theta_lr, theta_lr_dot, theta_b, theta_b_dot]
+        /// @param x_d = [s, s_dot, s_ddot, phi, phi_dot, phi_ddot, theta_ll, theta_ll_dot, theta_lr, theta_lr_dot, theta_b, theta_b_dot]
         /// @param psi_d 
         /// @param l_d leg length
-        void set_refinput(float x_d[10], float psi_d, float l_d);
+        void set_refinput(float x_d[12], float psi_d, float l_d);
 
         /// @brief fdb input from the observer
-        /// @param x = [s, s_dot, phi, phi_dot, theta_ll, theta_ll_dot, theta_lr, theta_lr_dot, theta_b, theta_b_dot]
+        /// @param x = [s, s_dot, s_ddot, phi, phi_dot, phi_ddot, theta_ll, theta_ll_dot, theta_lr, theta_lr_dot, theta_b, theta_b_dot]
         /// @param psi 
         /// @param ll left leg length
         /// @param lr right leg length
         /// @param jl jacobian matrix for left
         /// @param jr Jacobian matrix for right
         /// @param a_z 
-        void set_fdbinput(float x[10], float psi, float ll, float lr, float jl[4], float jr[4], float a_z);
+        void set_fdbinput(float x[12], float psi, float ll, float lr, float jl[4], float jr[4], float a_z);
         /// @brief calculate the output and send them to the Can_bus
         /// @param output for balancing contorl the data form is [T_lwl, T_jlf, T_jlb, T_lwr, T_jrf, T_jrb] 
         void step(float output[NUM_MOTORS]);
