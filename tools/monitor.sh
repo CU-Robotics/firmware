830438f3ff0073c2eb6b546961ede502f9a7c6b2
StudentKyle <studentkyles@gmail.com>
2024-09-20 13:46:52 -0600

    Move tty id finding to get_tty_path.sh instead of having duplicate code in monitor.sh and prepare_gdb.sh

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -1,5 +1,4 @@
-# path to the /dev/ serial device that the Teensy is connected to
-tty_path=""
+#!/bin/bash
 
 # handle sigint in a strange way to not break tycmd monitor
 trap 'exit 0' INT;
@@ -9,18 +8,7 @@ trap 'exit 0' INT;
 # where * is a number
 # The important part is the "if00" at the end, its interface number 00
 
-# Find the correct serial device
-for i in /dev/serial/by-id/*; do
-	if [[ "$i" == *-if00 ]]; then
-		tty_path=$i
-		break
-	fi
-done
-
-# found the id, now we need the actual path to the specific ttyACM device
-if [ -n "$tty_path" ]; then
-	tty_path=$(readlink -f $tty_path)
-fi
+tty_path=$(./tools/get_tty_path.sh *-if00)
 
 # If the tty path is not empty, we can start the monitor
 if [ -n "$tty_path" ]; then
@@ -32,8 +20,7 @@ if [ -n "$tty_path" ]; then
 	exit 0
 else
 	# failed to find the correct serial device
-	echo "Failed to monitor: Could not find the correct serial device for Teensy"
-	echo "This probably means the Teensy is not connected or the Teensy is in bootloader mode (red LED on/blinking)"
+	echo "Failed to monitor."
 
 	exit 1
 fi
--- tools/prepare_gdb.sh
+++ tools/prepare_gdb.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This finds the correct ttyACM device and puts it into gdb_commands.txt
 
 # The correct serial device has the following name
@@ -6,20 +8,7 @@
 # The important part is the "if02" at the end, its interface number 02
 # SerialUSB1 is what interface 02 references
 
-tty_path=""
-
-# Find the correct serial device
-for i in /dev/serial/by-id/*; do
-	if [[ "$i" == *-if02 ]]; then
-		tty_path=$i
-		break
-	fi
-done
-
-# found the id, now we need the actual path to the specific ttyACM device
-if [ -n "$tty_path" ]; then
-	tty_path=$(readlink -f $tty_path)
-fi
+tty_path=$(./tools/get_tty_path.sh *-if02)
 
 # Now we put it into gdb_commands.txt
 if [ -n "$tty_path" ]; then
@@ -27,8 +16,7 @@ if [ -n "$tty_path" ]; then
 	
 	exit 0
 else
-	echo "Failed to find the correct serial device for Teensy"
-	echo "This probably means the Teensy is not connected or the Teensy is in bootloader mode (red LED on/blinking)"
+	echo "Failed to prepare gdb"
 
 	exit 1
 fi
