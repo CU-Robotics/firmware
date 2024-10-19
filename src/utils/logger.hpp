ba8eb60e195c8a20b67305ef48faffe86de5035a
Eric V <ericv555@gmail.com>
2024-10-19 16:30:04 -0600

    Fixed Doxygen comments

--- src/utils/logger.hpp
+++ src/utils/logger.hpp
@@ -13,17 +13,20 @@ public:
 	~Logger() = default;
 	
 	/// @brief Neccesary function to utilize Print abstract class
+	/// @params matches parameters in Print.h
 	size_t write(uint8_t b){print("UNEXPECTED PRINT IN LOGGER.HPP"); return b;}
 	
 	/// @brief copies internal buffer to inputted location (*data) in memory
 	/// @return number of bytes copied
+	/// @parems size of data and pointer to data
 	uint32_t grab_log_data(uint32_t size, uint8_t *data);
 private:
 	/// @brief copies formatted bytes to internal buffer
 	/// @return number of bytes
+	/// @parems pointer to a buffer and size of the buffer
 	size_t write(const uint8_t *buffer, size_t size);
 	
-	/// @brief amoount of bytes currently stored in log
+	/// @brief amount of bytes currently stored in log
 	/// @note also used as current position in memory
 	unsigned int cursor;
 };
