a980fba9132ad2c3486845b08259aa8b26e8716f
Jackson Stepka <jvsdestroyer25@gmail.com>
2024-11-26 17:55:30 -0700

    Implemented write

--- src/sensors/can/can_manager.cpp
+++ src/sensors/can/can_manager.cpp
@@ -6,7 +6,20 @@
 #include "can/C620.hpp"
 #include "can/MG8016EI6.hpp"
 
-void CanManager::init() {
+CANManager::CANManager() { }
+
+CANManager::~CANManager() {
+    // go through and delete any allocated motors
+    for (uint32_t motor = 0; motor < CAN_MAX_MOTORS; motor++) {
+        if (m_motors[motor] != nullptr) {
+            delete m_motors[motor];
+        }
+
+        m_motors[motor] = nullptr;
+    }
+}
+
+void CANManager::init() {
     // initialize CAN 1
     m_can1.begin();
     m_can1.setBaudRate(1000000u);   // 1Mbit baud
@@ -25,12 +38,12 @@ void CanManager::init() {
     for (uint32_t i = 0; i < CAN_MAX_MOTORS; i++) {
         if (m_motors[i] != nullptr)
             delete m_motors[i];
-        
+
         m_motors[i] = nullptr;
     }
 }
 
-void CanManager::configure(float motor_info[CAN_MAX_MOTORS][3]) {
+void CANManager::configure(float motor_info[CAN_MAX_MOTORS][3]) {
     // loop through all CAN_MAX_MOTORS
     for (uint32_t motor_id = 0; motor_id < CAN_MAX_MOTORS; motor_id++) {
         // grab the information for this specific motor
@@ -39,7 +52,7 @@ void CanManager::configure(float motor_info[CAN_MAX_MOTORS][3]) {
         int bus_id = (int)motor_info[motor_id][2];
 
         // if the controller type is 0, then this motor is unused and we should process the next one
-        if (controller_type == -1) {
+        if (controller_type == 0) {
             continue;
         }
         // else this motor is valid
@@ -64,9 +77,116 @@ void CanManager::configure(float motor_info[CAN_MAX_MOTORS][3]) {
             break;
         }
         default: {
-            Serial.printf("CanManager tried to create a motor of invalid type: %d\n", controller_type);
+            Serial.printf("CANManager tried to create a motor of invalid type: %d\n", controller_type);
             break;
         }
         }
+
+        // place the new motor in the array
+        m_motors[motor_id] = new_motor;
     }
+}
+
+void CANManager::write() {
+    // for each bus
+    for (uint32_t bus = 0; bus < CAN_NUM_BUSSES; bus++) {
+        // the c610s and c620s require combined messages so are treated differently
+        // I refer to them as rm motors as they are from RoboMaster
+        // first msg is for the first 4 motors, second msg is for the last 4 motors
+        // both the c610s and c620s can occupy the same message
+        CAN_message_t rm_motor_msgs[2];
+
+        // for each motor
+        for (uint32_t motor = 0; motor < CAN_MAX_MOTORS; motor++) {
+            // if the motor is null, skip it
+            if (m_motors[motor] == nullptr) {
+                continue;
+            }
+
+            // if the motor is on the wrong bus, skip it
+            if (m_motors[motor]->get_bus_id() != bus) {
+                continue;
+            }
+
+            // based on the motor type, figure out how to write the message
+            switch (m_motors[motor]->get_controller_type()) {
+            case C610_CONTROLLER:   // fallthrough
+            case C620_CONTROLLER: {
+                // depending on the motor ID, write the message to the correct msg in the array
+                if ((m_motors[motor]->get_id() - 1) / 4) {
+                    m_motors[motor]->write(rm_motor_msgs[1]);   // last 4 motors
+                } else {
+                    m_motors[motor]->write(rm_motor_msgs[0]);   // first 4 motors
+                }
+
+                // this combined message will be written to the bus after the motor loop
+
+                break;
+            }
+            case MG8016_CONTROLLER: {
+                CAN_message_t msg;
+                m_motors[motor]->write(msg);
+
+                // print buffer
+                Serial.printf("buf id %x: ", msg.id);
+                for (int i = 0; i < 8; i++) {
+                    Serial.printf("%02X ", msg.buf[i]);
+                }
+
+                Serial.println();
+
+                // the MG motors dont require msg merging so just write it to the bus
+
+                // write the message to the correct bus
+                if (bus == 0) {
+                    m_can1.write(msg);
+                } else if (bus == 1) {
+                    m_can2.write(msg);
+                } else if (bus == 2) {
+                    m_can3.write(msg);
+                } else {
+                    Serial.printf("CANManager tried to write to an invalid bus: %d\n", bus);
+                }
+
+                break;
+            }
+            default: {
+                Serial.printf("CANManager tried to write to a motor of invalid type: %d\n", m_motors[motor]->get_controller_type());
+                break;
+            }
+            }
+        }
+        
+        // write the combined messages to the bus
+        if (bus == CAN_1) {
+            m_can1.write(rm_motor_msgs[0]); // first 4 motors
+            m_can1.write(rm_motor_msgs[1]); // last 4 motors
+        } else if (bus == CAN_2) {
+            m_can2.write(rm_motor_msgs[0]);
+            m_can2.write(rm_motor_msgs[1]);
+        } else if (bus == CAN_3) {  // TODO: do we force only non-rm motors on can 3?
+            m_can3.write(rm_motor_msgs[0]);
+            m_can3.write(rm_motor_msgs[1]);
+        } else {
+            Serial.printf("CANManager tried to write to an invalid bus: %d\n", bus);
+        }
+    }
+
+}
+
+void CANManager::write_motor_torque(uint32_t motor_gid, float torque) {
+    // verify motor ID
+    if (motor_gid >= CAN_MAX_MOTORS) {
+        Serial.printf("CANManager tried to write to an invalid motor: %d\n", motor_gid);
+        return;
+    }
+
+    // verify motor exists
+    if (m_motors[motor_gid] == nullptr) {
+        Serial.printf("CANManager tried to write to an invalid motor: %d\n", motor_gid);
+        return;
+    }
+
+    // write the torque to the motor
+    m_motors[motor_gid]->write_motor_torque(torque);
 }
--- src/sensors/can/can_manager.hpp
+++ src/sensors/can/can_manager.hpp
@@ -4,16 +4,39 @@
 #include "motor.hpp"
 
 /// @brief The maximum number of motors that can be connected to the system. Derrived from the max motors per bus assuming bus 3 only holds non-RM motors
-constexpr uint32_t CAN_MAX_MOTORS = 8 + 8 + 32;
+constexpr uint32_t CAN_MAX_MOTORS = 8u + 8u + 32u;
+
+/// @brief The number of CAN busses that can be used
+constexpr uint32_t CAN_NUM_BUSSES = 3u;
+
+/// @brief CAN bus 1 ID
+constexpr uint32_t CAN_1 = 0u;
+/// @brief CAN bus 2 ID
+constexpr uint32_t CAN_2 = 1u;
+/// @brief CAN bus 3 ID
+constexpr uint32_t CAN_3 = 2u;
 
 // Because of how volatile FlexCAN_T4 objects are, there must only ever exist one instance of this class
 // I'm deleting any copy and assignment constructors that may act to create a new instance
 
-class CanManager {
+class CANManager {
 public:
-    CanManager() = default;
+    CANManager();
+
+    /// @brief Deleted copy and assignment constructors. This class should only ever have one instance
+    /// @param copy The CANManager object to copy
+    CANManager(const CANManager& copy) = delete;
+    /// @brief Deleted copy and assignment constructors. This class should only ever have one instance
+    /// @param copy The CANManager object to copy
+    /// @return CANManager& The reference to this object
+    CANManager& operator=(const CANManager&) = delete;
+
+    /// @brief Destructor, cleans up motor array
+    ~CANManager();
 
 public:
+    /// @brief Initialize the CAN buses and motor array
+    /// @note This can be called multiple times in the event of hot reloading
     void init();
 
     /// @brief Dynamically create the motor objects based on config data
@@ -24,16 +47,24 @@ public:
 
     void write();
 
+    /// @brief Write a torque command to a specific motor given it's global ID
+    /// @param motor_gid The global ID of the motor to write to
+    /// @param torque The normalized torque value to write to the motor in the range [-1, 1]
+    /// @note This does not issue a CAN command over the bus
     void write_motor_torque(uint32_t motor_gid, float torque);
 
 private:
 
 
 private:
+    /// @brief CAN bus 1
     FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> m_can1;
+    /// @brief CAN bus 2
     FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> m_can2;
+    /// @brief CAN bus 3
     FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> m_can3;
 
+    /// @brief Array of generic motor objects
     Motor* m_motors[CAN_MAX_MOTORS] = { nullptr };
 
 };
