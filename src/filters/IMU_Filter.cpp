4c461aeabfc029b1772c3d0eb14255b900b09381
JimOAOOO <yili4413@colorado.edu>
2024-11-28 04:13:57 -0700

    IMUFilter_Done

--- src/filters/IMU_Filter.cpp
+++ src/filters/IMU_Filter.cpp
@@ -7,10 +7,10 @@ void IMU_filter::init(){
     _icm.init(_icm.SPI);
     _icm.set_gyro_range(4000);
     calibrate_imu();
-
+    _alpha = 0.9985;
 }
 
-void IMU_filter::read(){
+void IMU_filter::read() {
     _icm.read();
     _imu.accel_X = _icm.get_accel_X() * _imu.accel_scale;
     _imu.accel_Y = _icm.get_accel_Y() * _imu.accel_scale; 
@@ -18,44 +18,56 @@ void IMU_filter::read(){
     _imu.alpha_X = _icm.get_gyro_X();
     _imu.alpha_Y = _icm.get_gyro_Y();
     _imu.alpha_Z = _icm.get_gyro_Z();
+    _imu.acclangle_Y = -atanf(_imu.accel_X / sqrt(_imu.accel_Y * _imu.accel_Y + _imu.accel_Z * _imu.accel_Z));
+    _imu.acclangle_X = atanf(_imu.accel_Y / sqrt(_imu.accel_X * _imu.accel_X + _imu.accel_Z * _imu.accel_Z));
+
     float dt = timer.delta();
-    _imu.angle_X += dt * _icm.get_gyro_X(); 
-    _imu.angle_Y += dt * _icm.get_gyro_Y();
+    _alpha = 0.9985 - (0.0005 * SENSORS_GRAVITY_EARTH/sqrt((_imu.accel_X*_imu.accel_X) + (_imu.accel_Y*_imu.accel_Y) + (_imu.accel_Z*_imu.accel_Z)));
+    float realalpha_X = _imu.alpha_X * cos(_imu.angle_Y) + _imu.alpha_Z * sin(_imu.angle_Y);
+    float realalpha_Y = _imu.alpha_Y * cos(_imu.angle_X) - _imu.alpha_Z * sin(_imu.angle_X);    
+
+    _imu.angle_X = _alpha * (_imu.angle_X + realalpha_X * dt) + (1.0 - _alpha) * _imu.acclangle_X;
+    _imu.angle_Y = _alpha * (_imu.angle_Y + realalpha_Y * dt) + (1.0 - _alpha) * _imu.acclangle_Y;
+
+    
     _imu.temperature = _icm.get_temperature();
-//Something somthing need to be test
 }
 
-void IMU_filter::print(){
-    // Display the temperature data, measured in Celcius
-	Serial.print("\t\tTemperature ");
-	Serial.print(_imu.temperature);
-	Serial.println(" deg C");
-	// Display the acceleration data, measured in m/s^2)
-	Serial.print("\t\tAccel X: ");
-	Serial.print(_imu.accel_X);
-	Serial.print(" \tY: ");
-	Serial.print(_imu.accel_Y);
-	Serial.print(" \tZ: ");
-	Serial.print(_imu.accel_Z);
-	Serial.println(" m/s^2 ");
-	// Display gyroscope data, measured in radians/s
-	Serial.print("\t\tGyro X: ");
-	Serial.print(_imu.alpha_X);
-	Serial.print(" \tY: ");
-	Serial.print(_imu.alpha_Y);
-	Serial.print(" \tZ: ");
-	Serial.print(_imu.alpha_Z);
-	Serial.println(" radians/s ");
-	Serial.println(_imu.angle_Y);
-    Serial.println("_imu.angle_Y");
+void IMU_filter::print() {
+    Serial.print("\t\tTemperature ");
+    Serial.print(_imu.temperature);
+    Serial.println(" deg C");
+
+    Serial.print("\t\tAccel X: ");
+    Serial.print(_imu.accel_X);
+    Serial.print(" \tY: ");
+    Serial.print(_imu.accel_Y);
+    Serial.print(" \tZ: ");
+    Serial.print(_imu.accel_Z);
+    Serial.println(" m/s^2 ");
+
+    Serial.print("\t\tGyro X: ");
+    Serial.print(_imu.alpha_X);
+    Serial.print(" \tY: ");
+    Serial.print(_imu.alpha_Y);
+    Serial.print(" \tZ: ");
+    Serial.print(_imu.alpha_Z);
+    Serial.println(" radians/s ");
+
     Serial.println(_imu.angle_X);
     Serial.println("_imu.angle_X");
-}   
+    Serial.println(_imu.angle_Y);
+    Serial.println("_imu.angle_Y");
+
+    Serial.println(_imu.acclangle_X);
+    Serial.println("_imu.acclangle_X");
+    Serial.println(_imu.acclangle_Y);
+    Serial.println("_imu.acclangle_Y");
+}
 
 IMUData* IMU_filter::getdata(){
     return &_imu;
 }
-
 void IMU_filter::calibrate_imu(){
     Serial.println("Calibrating IMU...");
     float sum_accel_x = 0;
@@ -81,5 +93,7 @@ void IMU_filter::calibrate_imu(){
     _imu.angle_Y=-atanf(x / sqrt(y*y + z*z));
     _imu.angle_X=atanf(y / sqrt(x*x + z*z));
 
-
 }
+
+
+
--- src/filters/IMU_Filter.hpp
+++ src/filters/IMU_Filter.hpp
@@ -2,19 +2,21 @@
 #define IMU_FILTER_H
 #include "../sensors/ICM20649.hpp"
 #include "../utils/timing.hpp"
+
 #define ACCEL_SENSITIVITY 
 #define CALIBRATION_NUM 100000
 struct IMUData{
     float accel_X;
     float accel_Y;
     float accel_Z; 
-    float alpha_X; // Angular acceleration left and right
-    float alpha_Y; // front and back
-    float alpha_Z; // Rotation 
-    float angle_X; // right(+) and left(-) --flat is 0 
-    float angle_Y; // front(+) and back(-) --flat is 0
+    float alpha_X; // Roll axis
+    float alpha_Y; // pitch axis
+    float alpha_Z; // Yaw axis 
+    float angle_X; // Roll axis
+    float angle_Y; // pitch axis
+    float acclangle_X; // Roll axis
+    float acclangle_Y; // pitch axis 
     float temperature;
-
     float accel_scale;
 };
 class IMU_filter{
@@ -22,6 +24,8 @@ class IMU_filter{
         ICM20649 _icm;
 
         IMUData _imu;
+
+        float _alpha;
 
         Timer timer;
     public:
