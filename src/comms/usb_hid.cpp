6d061f1b41df00cecf6189c45b7ccd6c27337355
ashleycody345 <ashley.cody@colorado.edu>
2024-12-03 23:40:23 -0700

    renamed CommsPacket to HIDPacket for consistency with ethernet

--- src/comms/usb_hid.cpp
+++ src/comms/usb_hid.cpp
@@ -1,52 +1,52 @@
 #include "usb_hid.hpp"
 
-uint8_t CommsPacket::get_id() {
+uint8_t HIDPacket::get_id() {
     // c++ moment
     char* x = raw;
     return *reinterpret_cast<uint8_t*>(x + TEENSY_PACKET_ID_OFFSET);
 }
 
-uint8_t CommsPacket::get_info() {
+uint8_t HIDPacket::get_info() {
     return *reinterpret_cast<uint8_t*>(raw + TEENSY_PACKET_INFO_OFFSET);
 }
 
-void CommsPacket::set_id(uint16_t id) {
+void HIDPacket::set_id(uint16_t id) {
     // c++ moment
     char* x = raw;
     *reinterpret_cast<uint16_t*>(x + TEENSY_PACKET_ID_OFFSET) = id;
 }
 
-void CommsPacket::set_info(uint8_t info) {
+void HIDPacket::set_info(uint8_t info) {
     *reinterpret_cast<uint8_t*>(raw + TEENSY_PACKET_INFO_OFFSET) = info;
 }
 
-void CommsPacket::get_target_state(float state[STATE_LEN][3]) {
+void HIDPacket::get_target_state(float state[STATE_LEN][3]) {
     memcpy(state, raw + KHADAS_PACKET_TSTATE_OFFSET, sizeof(float) * STATE_LEN * 3);
 }
 
-uint8_t CommsPacket::get_hive_override_request() {
+uint8_t HIDPacket::get_hive_override_request() {
     return *reinterpret_cast<uint8_t*>(raw + KHADAS_PACKET_HIVE_OVERRIDE_STATE_REQUEST_OFFSET);
 }
 
-void CommsPacket::get_hive_override_state(float state[STATE_LEN][3]) {
+void HIDPacket::get_hive_override_state(float state[STATE_LEN][3]) {
     memcpy(state, raw + KHADAS_PACKET_HIVE_OVERRIDE_STATE_OFFSET, sizeof(float) * STATE_LEN * 3);
 }
 
-void CommsPacket::get_ref_draw_data(char** draw_data) {}
+void HIDPacket::get_ref_draw_data(char** draw_data) {}
 
-void CommsPacket::set_time(double time) {
+void HIDPacket::set_time(double time) {
     memcpy(raw + TEENSY_PACKET_TIME_OFFSET, &time, sizeof(double));
 }
 
-void CommsPacket::set_estimated_state(float state[STATE_LEN][3]) {
+void HIDPacket::set_estimated_state(float state[STATE_LEN][3]) {
     memcpy(raw + TEENSY_PACKET_ESTATE_OFFSET, state, sizeof(float) * STATE_LEN * 3);
 }
 
-void CommsPacket::set_sensor_data(SensorData* sensor_data) {
+void HIDPacket::set_sensor_data(SensorData* sensor_data) {
     memcpy(raw + TEENSY_PACKET_SENSOR_OFFSET, sensor_data->raw, sizeof(SensorData));
 }
 
-void CommsPacket::set_ref_data(uint8_t ref_data[180]) {
+void HIDPacket::set_ref_data(uint8_t ref_data[180]) {
     memcpy(raw + TEENSY_PACKET_REF_OFFSET, ref_data, 180);
 }
 
