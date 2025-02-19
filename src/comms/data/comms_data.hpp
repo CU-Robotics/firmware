3b95cd8a3c8a3b3d3c8cef7b99cd368a6a5f47c6
Pandabear1125 <jvsdestroyer25@gmail.com>
2025-02-19 13:17:45 -0700

    Adds hive's comms stuff

--- src/comms/data/comms_data.hpp
+++ src/comms/data/comms_data.hpp
@@ -1,200 +1,53 @@
 #pragma once
 
-#include <cstdint>
-#include "../../controls/controller.hpp"
-
-#define NUM_SENSOR_VALUES 12
-#define NUM_SENSORS 16
-
-// RobotState stuff
-/// @brief Number of state
-constexpr int	ROBOT_STATE_LENGTH = 24u;
-/// @brief Number of types of state (position, velocity, acceleration, ...)
-constexpr int	ROBOT_STATE_DERIVATIVES_COUNT = 3u;
-
-/// @brief Index of position in robot state
-constexpr int 	ROBOT_STATE_POSITION = 0u;
-/// @brief Index of velocity in robot state
-constexpr int 	ROBOT_STATE_VELOCITY = 1u;
-/// @brief Index of acceleration in robot state
-constexpr int 	ROBOT_STATE_ACCELERATION = 2u;
-
-
+#include <cstdint>          // for uintN_t
 
 namespace Comms {
 
-
-/// @brief Label for a particular piece of data derived from CommsData.
+/// @brief TypeLabel is a unique identifier for each type of data that can be sent over comms.
 enum class TypeLabel : uint8_t {
-    // default type
     NONE = 0x00,
-
-    // Hive types
-    TargetState = 0x11,
-    OverrideState = 0x22,
-    LoggingData = 0x33,
-
-    // Firmware types
-    FWSample1 = 0x77,
-    FWSample2 = 0x88,
-    FWSample3 = 0x99,
-
-    // TODO rest of types for final data structs
+    TestData,
+    LoggingData,
 };
 
-/// @brief Label to describe the physical implementation a piece of data needs to go through.
+/// @brief PhysicalMedium is the medium over which the data is sent.
 enum class PhysicalMedium : uint8_t {
-    HID,
-    Ethernet
+    HID = 0x00,
+    Ethernet,
 };
 
-/// @brief Label to describe the priority of a piece of data (determines what order it will be sent in with regards to everything else)
+/// @brief Priority is the priority of the data being sent.
 enum class Priority : uint8_t {
-    High,
+    High = 0x00,
     Medium,
+    Logging,
 };
 
-/// @brief Base class that all data that is sent over comms must inherit from.
+
+/// @brief base class for all data structs that want to be sent over comms.
 struct CommsData {
 public:
-    /// @brief Generic constructor for unspecified CommsData object
-    CommsData() {
-        CommsData(sizeof(CommsData), TypeLabel::NONE, Priority::High);
-    }
-    /// @brief General constructor for CommsData with a given type, priority, and size
-    /// @param size Size of derived object
-    /// @param type_label Type of derived object
-    /// @param priority Priority of transmitting derived object
-    CommsData(uint16_t size, TypeLabel type_label, Priority priority) {
+    /// @brief Primary constructor, initializes all fields.
+    /// @param type_label The type of data being sent.
+    /// @param physical_medium The medium over which the data is sent.
+    /// @param priority The priority of the data being sent.
+    /// @param size The size of the data in bytes.
+    CommsData(TypeLabel type_label, PhysicalMedium physical_medium, Priority priority, uint16_t size) {
         this->type_label = type_label;
-        // this->physical_medium = physical_medium;
+        this->physical_medium = physical_medium;
         this->priority = priority;
         this->size = size;
     }
 
-    /// @brief Size of this object
-    uint16_t size = 0;
-
-    /// @brief Type label for this object
+    /// @brief size of the data in bytes
+    uint16_t size : 16;
+    /// @brief type of data
     TypeLabel type_label : 8;
-
-    // PhysicalMedium physical_medium : 4 = PhysicalMedium::HID;
-
-    /// @brief Priority of this object
+    /// @brief medium over which the data is sent
+    PhysicalMedium physical_medium : 4;
+    /// @brief priority of the data
     Priority priority : 4;
 };
-
-
-// literally JUST a config object right now. 
-struct ConfigData {
-    // /// @brief robot id
-    // float robot;
-
-    // /// @brief matrix that defines type and neccessary values for each sensor
-    // float sensor_info[NUM_SENSORS][NUM_SENSOR_VALUES + 1];
-
-
-    // /// @brief gains matrix
-    // float gains[NUM_ROBOT_CONTROLLERS][NUM_GAINS];
-    // /// @brief gear ratio matrix
-    // float gear_ratios[NUM_ROBOT_CONTROLLERS][NUM_MOTORS];
-
-    // /// @brief matrix that contains the type, physical id, and physical bus of each motor
-    // float motor_info[NUM_MOTORS][3];
-    // /// @brief reference limits matrix
-    // float set_reference_limits[STATE_LEN][3][2];
-    
-    // /// @brief the estimator id's and info
-    // float estimator_info[NUM_ESTIMATORS][STATE_LEN + 1];
-    // /// @brief controller id's and info
-    // float controller_info[NUM_ROBOT_CONTROLLERS][NUM_MOTORS + 1];
-    // /// @brief governor types
-    // float governor_types[STATE_LEN];
-};
-
-// Hive struct types
-
-/// @brief Struct containing the full robot state including time
-struct RobotState : public CommsData{
-    /// @brief Default constructor
-    RobotState() = default;
-
-    /// @brief Construct a RobotState object with raw byte arrays for time and state
-    /// @param raw_time Raw byte array representing time
-    /// @param raw_state Raw byte array representing state
-    RobotState(char* raw_time, char* raw_state) {
-        memcpy(&time, raw_time, sizeof(double));
-        memcpy(&state, raw_state, sizeof(RobotState::state));
-    }
-
-    // RobotState(const RobotState &robot_state){
-    // 	memcpy(&time,  &robot_state.time, sizeof(double));
-    // 	memcpy(state, robot_state.state, sizeof(RobotState::state));
-    // }
-    /// @brief Time of the teensy
-    double time = 0.0;
-    /// @brief Full robot state array
-    float state[ROBOT_STATE_LENGTH][ROBOT_STATE_DERIVATIVES_COUNT] = { 0 };
-    /// @brief The delay in communication between the teensy and the khadas
-    double comms_delay = 0;
-};
-
-
-
-// Firmware struct types
-
-/// @brief Sample data struct for FirmwareData
-struct FWSample1 : public CommsData {
-    /// @brief constructor initializing CommsData
-    FWSample1() : CommsData(sizeof(FWSample1), TypeLabel::FWSample1, Priority::Medium) { }
-
-    /// @brief Sample number
-    uint32_t num;
-};
-
-/// @brief Sample data struct for FirmwareData
-struct FWSample2 : public CommsData {
-    /// @brief constructor initializing CommsData
-    FWSample2() : CommsData(sizeof(FWSample2), TypeLabel::FWSample2, Priority::Medium) { }
-
-    /// @brief Sample number
-    uint32_t num;
-};
-
-/// @brief Sample data struct for FirmwareData
-struct FWSample3 : public CommsData {
-    /// @brief constructor initializing CommsData
-    FWSample3() : CommsData(sizeof(FWSample3), TypeLabel::FWSample3, Priority::Medium) { }
-
-    /// @brief Sample number
-    uint32_t num;
-};
-
-
-// Megastruct defs
-
-/// @brief Object containing all of the data types that are sent over comms, which specifically come from Firmware and go to Hive.
-struct FirmwareData {
-    // TODO: define structs for firmware outgoing data
-
-    /// @brief Sample data 1
-    FWSample1 sample1;
-
-    /// @brief Sample data 2
-    FWSample2 sample2;
-
-    /// @brief Sample data 3
-    FWSample3 sample3;
-};
-
-/// @brief Object containing all of the data types that are sent over comms, which specifically come from Hive and go to Firmware.
-struct HiveData {
-    /// @brief Target state data
-    RobotState target_state;
-
-    /// @brief Override state data
-    RobotState override_state;
-};
-
 
 } // namespace Comms
--- src/comms/data/packet_payload.hpp
+++ src/comms/data/packet_payload.hpp
@@ -1,83 +1,430 @@
 #pragma once
 
-#include <queue>
-#include <string.h>
+#include <memory>               // for shared_ptr
+#include <queue>                // for queue
 
-#include "comms_data.hpp"
+#if defined(HIVE)
+#include <exception>            // for runtime_error
+#elif defined(FIRMWARE)
+#include <cassert>              // for assert
+#endif
 
-#define MAX_QUEUE_SIZE 32
+#if defined(HIVE)
+#include <doctest/doctest.h>                    // for TEST_CASE
+#include "modules/comms/data/comms_data.hpp"    // for CommsData
+#include "modules/comms/data/logging_data.hpp"  // for LoggingData
+#include "modules/comms/data/firmware_data.hpp" // for FirmwareData
+#include "modules/hive/environment.hpp"         // for Hive
+#elif defined(FIRMWARE)
+#include "comms/data/comms_data.hpp"            // for CommsData
+#include "comms/data/logging_data.hpp"          // for LoggingData
+#include "comms/data/firmware_data.hpp"         // for FirmwareData
+#endif
 
 namespace Comms {
 
 /// @brief Constructs and stores the data part of a packet we want to send over a physical layer.
 class PacketPayload {
 public:
-    /// @brief Default constructor for PacketPayload
+    /// @brief Default constructor.
     PacketPayload() = default;
 
-    /// @brief Constructs PacketPayload by allocating raw_data to store packet data, and configuring a max size for a data type.
-    /// @param max_data_size Maximum size of the data we can store in a packet
+    /// @brief Constructor, allocates the raw data buffer.
+    /// @param max_data_size The maximum size of the data part of the packet.
     PacketPayload(uint16_t max_data_size);
 
-    /// @brief Destructor for PacketPayload, deallocates memory used by raw_data buffer.
+    /// @brief Destructor, cleans up the raw data buffer.
     ~PacketPayload();
 
-    /// @brief Assembles the data in queues to create a sendable sequence over a PhysicalMedium.
-    /// @note The structure of this payload is as follows: a CommsData derived object is inserted, with the component
-    /// @note identifiable as CommsData appearing first. This data can be read to retrieve a TypeLabel and a size,
-    /// @note which can be used to navigate to the next element in the payload, as well as copy the data out into
-    /// @note a particular type. This is placed into a packet, which gets deconstructed as part of the CommsLayer::decode() routine.
+public:
+    /// @brief Add a CommsData to the correct queue.
+    /// @param data The CommsData to add.
+    void add(CommsData* data);
+
+    /// @brief Using the CommsDatas in the queues, construct a complete data packet.
+    /// @note Access this output using the data() function.
     void construct_data();
 
-    // void deconstruct_data();    // TODO
+    /// @brief Deconstructs the data packet. Places each CommsData into the correct place in the mega structs  
+    /// @param data The raw data buffer.
+    /// @param size The size of the raw data buffer.
+    void deconstruct_data(uint8_t* data, uint16_t size);
 
-    /// @brief Adds a pre-configured piece of data to the PacketPayload queue, to be assembled into a packet later.
-    /// @param data Pointer to the data that we are appending.
-    /// @note This function will automatically free the data pointed to, assuming it is heap-allocated.
-    /// @note This function should, in general, only be called by the send() method of the Container template class.
-    /// @note *data pointer MUST be heap allocated, otherwise you WILL cause invalid free errors!!!!!*
-    bool add(CommsData* data);
-
-    /// @brief Returns a pointer to the raw constructed data after construct_data() is called.
-    /// @return Pointer to the raw byte array
+    /// @brief Get the raw data buffer.
+    /// @return The raw data buffer.
+    /// @note This is a complete data packet.
     uint8_t* data();
 
-    /// @brief Returns the size of data currently in use in the raw data buffer.
-    /// @return Size of the data in the data buffer
-    uint16_t size();
 private:
-    /// @brief Empties the contents of the raw data buffer (sets all entries to 0)
-    void clear();
+    /// @brief Clear the raw data buffer.
+    void clear_raw_data();
 
-    /// @brief Attempts to append all of the data in a queue to the raw byte buffer, stopping when the queue is empty or the buffer is full.
-    /// @param queue The queue of CommsData pointers to read from
+    /// @brief Append as much data from a queue to the raw data buffer as possible.
+    /// @param queue The queue to append data from.
     void append_data_from_queue(std::queue<CommsData*>& queue);
 
-    /// @brief Attempts to append a single piece of data to the raw byte buffer.
-    /// @param data A pointer to a single CommsData derived object to append
-    /// @return True on success, otherwise false
+    /// @brief Try to append a CommsData to the raw data buffer.
+    /// @param data The CommsData to append.
+    /// @return True if the data was appended, false if it could not be appended.
     bool try_append_data(CommsData* data);
 
-    // void fill_logging_data_from_queue(std::queue <LoggingData*>& logging_queue);
+    /// @brief Fill the raw data buffer with LoggingData.
+    /// @param logging_queue The queue to fill the raw data buffer with.
+    /// @note This is a distinct function because LoggingData has a dynamic size AND it can be split across multiple packets.
+    void fill_logging_data_from_queue(std::queue<LoggingData*>& logging_queue);
 
-    // bool try_append_splittable_logging_data(LoggingData& log);
+    /// @brief Try to append a LoggingData to the raw data buffer.
+    /// @param log The LoggingData to append.
+    /// @return True if the data was appended, false if it could not be appended.
+    /// @note This is a distinct function because LoggingData has a dynamic size AND it can be split across multiple packets.
+    bool try_append_splittable_logging_data(LoggingData* log);
 
-    /// @brief Queue of data objects to send with high priority (this is sent until it is empty first)
-    std::queue <CommsData*> high_priority_send_queue;
-    /// @brief Queue of data objects to send with medium prority (this will only be sent if high priority queue is empty)
-    std::queue <CommsData*> medium_priority_send_queue;
-    // std::queue <LoggingData*> logging_send_queue;
+    /// @brief Place the data in the mega struct.
+    /// @param data The CommsData to place in the mega struct.
+    void place_data_in_mega_struct(CommsData* data);
 
-    /// @brief Remaining size in the data buffer to write to
-    /// @note this type means our max packet size is 2^16-1 bytes long.
-    uint16_t remaining_size = 0;
+private:
+    /// @brief The high priority send queue.
+    std::queue<CommsData*> high_priority_send_queue;
+    /// @brief The medium priority send queue.
+    std::queue<CommsData*> medium_priority_send_queue;
+    /// @brief The logging send queue.
+    std::queue<LoggingData*> logging_send_queue;
 
-    /// @brief Pointer to the raw byte array
+    /// @brief The raw data buffer. This is the complete data packet.
     uint8_t* raw_data = nullptr;
-
-    /// @brief Total amount of data we can fit into a single payload.
+    /// @brief The maximum size of this data packet.
     uint16_t max_data_size = 0;
+    /// @brief The remaining size of the data packet.
+    uint16_t remaining_data_size = 0;
 };
 
+}   // namespace Comms
 
+#ifdef HIVE
+
+TEST_CASE("Testing packet payload") {
+
+    // Create a packet payload with a max size of 103 bytes
+    // 103 was chosen because it is 3 bytes more than the any multiple size of the test data object
+    // 3 is also less then the size of the CommsData header
+    constexpr int PACKET_SIZE = 103;
+    Comms::PacketPayload payload(PACKET_SIZE);
+    Comms::PacketPayload default_payload;   // "tests" the default constructor
+
+    // Create a test data object
+    TestData data;
+    data.x = 1.5f;
+    data.y = 2.5f;
+    data.z = 3.5f;
+    data.w = 0x12345678;
+
+    SUBCASE("Testing payload fills raw data") {
+        payload.add(new TestData(data));
+        
+        payload.construct_data();
+
+        uint8_t* raw_data = payload.data();
+        uint8_t* test_data_ptr = reinterpret_cast<uint8_t*>(&data);
+
+        REQUIRE(memcmp(raw_data, test_data_ptr, sizeof(TestData)) == 0);
+    }
+
+    SUBCASE("Testing payload pops data") {
+        payload.add(new TestData(data));
+
+        // construct once, the data should be in the raw data buffer
+        payload.construct_data();
+        // construct again, this clears the buffer, and our previously added object should be popped
+        payload.construct_data();
+
+        uint8_t empty_buffer[sizeof(TestData)];
+        memset(empty_buffer, 0, sizeof(TestData));
+
+        uint8_t* raw_data = payload.data();
+
+        // ensure the raw data buffer is now empty
+        REQUIRE(memcmp(raw_data, empty_buffer, sizeof(TestData)) == 0);
+    }
+
+    SUBCASE("Testing fill too much data does not crash") {
+        // fill the payload with too much data
+        // this adds 1 more than the max size of the payload
+        for (size_t i = 0; i < PACKET_SIZE / sizeof(TestData) + 1; i++) {
+            payload.add(new TestData(data));
+        }
+
+        // construct the data, this should not crash
+        payload.construct_data();
+    }
+
+    SUBCASE("Testing fill to much data still leaves data in queue") {
+        // fill the payload with too much data
+        // this adds 1 more than the max size of the payload
+        for (size_t i = 0; i < PACKET_SIZE / sizeof(TestData) + 1; i++) {
+            payload.add(new TestData(data));
+        }
+
+        // construct the data, this should not crash
+        payload.construct_data();
+
+        // construct the data again, this should not crash
+        payload.construct_data();
+
+        uint8_t* raw_data = payload.data();
+        uint8_t empty_buffer[sizeof(TestData)];
+        memset(empty_buffer, 0, sizeof(TestData));
+
+        // ensure the raw data buffer is not empty
+        REQUIRE(memcmp(raw_data, empty_buffer, sizeof(TestData)) != 0);
+    }
+
+    SUBCASE("Testing different priority data") {
+        TestData high_priority_data;
+        high_priority_data.x = 1.5f;
+        high_priority_data.y = 2.5f;
+        high_priority_data.z = 3.5f;
+        high_priority_data.w = 0x12345678;
+
+        TestData medium_priority_data;
+        medium_priority_data.priority = Comms::Priority::Medium;
+        medium_priority_data.x = 4.5f;
+        medium_priority_data.y = 5.5f;
+        medium_priority_data.z = 6.5f;
+        medium_priority_data.w = 0x87654321;
+        
+        Comms::LoggingData logging_data;
+        char log_buffer[] = "This is a test log message.";
+        logging_data.deserialize(log_buffer, sizeof(log_buffer));
+
+        payload.add(new TestData(medium_priority_data));
+        payload.add(new TestData(high_priority_data));
+        payload.add(new Comms::LoggingData(logging_data));
+
+        payload.construct_data();
+
+        uint8_t* raw_data = payload.data();
+
+        // ensure the high priority data is in the buffer first
+        REQUIRE(memcmp(raw_data, &high_priority_data, sizeof(TestData)) == 0);
+
+        // ensure the medium priority data is in the buffer next
+        raw_data += sizeof(TestData);
+        REQUIRE(memcmp(raw_data, &medium_priority_data, sizeof(TestData)) == 0);
+
+        // ensure the logging data is in the buffer last
+        raw_data += sizeof(TestData) + sizeof(Comms::CommsData);
+        Comms::LoggingData logging_data_out;
+        logging_data_out.deserialize((char*)raw_data, sizeof(log_buffer));
+        REQUIRE(logging_data_out.get_logs() == logging_data.get_logs());
+    }
+
+    SUBCASE("Testing adding invalid priority data") {
+        TestData invalid_priority_data;
+        invalid_priority_data.priority = static_cast<Comms::Priority>(15);
+
+        REQUIRE_THROWS_AS(payload.add(new TestData(invalid_priority_data)), std::runtime_error);
+    }
+
+    SUBCASE("Testing adding logging data with not enough space available") {
+        Comms::LoggingData logging_data;
+        char log_buffer[] = "This is a test log message.";
+        logging_data.deserialize(log_buffer, sizeof(log_buffer));
+
+        // this adds the most payloads as possible, leaves a little bit of room left
+        for (size_t i = 0; i < PACKET_SIZE / sizeof(TestData); i++) {
+            payload.add(new TestData(data));
+        }
+
+        // add the logging data
+        payload.add(new Comms::LoggingData(logging_data));
+
+        // construct the data, this should not crash
+        payload.construct_data();
+
+        // construct the data again, this should not crash
+        payload.construct_data();
+
+        uint8_t* raw_data = payload.data();
+        uint8_t empty_buffer[sizeof(TestData)];
+        memset(empty_buffer, 0, sizeof(TestData));
+
+        // ensure the raw data buffer is not empty
+        REQUIRE(memcmp(raw_data, empty_buffer, sizeof(TestData)) != 0);
+
+        // ensure the logging data is in the buffer
+        Comms::LoggingData logging_data_out;
+        raw_data += sizeof(Comms::CommsData);
+        logging_data_out.deserialize((char*)raw_data, sizeof(log_buffer));
+        REQUIRE(logging_data_out.get_logs() == logging_data.get_logs());
+    }
+
+    SUBCASE("Testing adding partial logging data") {
+        Comms::LoggingData logging_data;
+        char log_buffer[] = "This is a test log message.";
+        logging_data.deserialize(log_buffer, sizeof(log_buffer));
+
+        // fill the payload with 1 less than the max size of the payload
+        for (size_t i = 0; i < PACKET_SIZE / sizeof(TestData) - 1; i++) {
+            payload.add(new TestData(data));
+        }
+
+        int test_data_added = (PACKET_SIZE / sizeof(TestData) - 1);
+
+        // add the logging data
+        payload.add(new Comms::LoggingData(logging_data));
+
+        // construct the data, this should not crash
+        payload.construct_data();
+
+        // verify there are test_data_added instances of the test data in the buffer
+        uint8_t* raw_data = payload.data();
+        uint8_t* test_data_ptr = reinterpret_cast<uint8_t*>(&data);
+        for (int i = 0; i < test_data_added; i++) {
+            REQUIRE(memcmp(raw_data, test_data_ptr, sizeof(TestData)) == 0);
+            raw_data += sizeof(TestData);
+        }
+
+        // ensure there some logging data in the buffer
+        Comms::LoggingData logging_data_out1;
+        raw_data += sizeof(Comms::CommsData);
+        logging_data_out1.deserialize((char*)raw_data, sizeof(log_buffer));
+        // this part of the logging data should not be empty
+        REQUIRE(logging_data_out1.get_logs() != "");
+
+        // construct the data again, this should not crash
+        payload.construct_data();
+
+        // verify that the rest of the logging data is in the buffer
+        raw_data = payload.data();
+
+        Comms::LoggingData logging_data_out2;
+        raw_data += sizeof(Comms::CommsData);
+        logging_data_out2.deserialize((char*)raw_data, sizeof(log_buffer));
+        // this part of the logging data should not be empty
+        REQUIRE(logging_data_out2.get_logs() != "");
+
+        // ensure that all the logging data was eventually grabbed
+        REQUIRE(logging_data_out1.get_logs() + logging_data_out2.get_logs() == logging_data.get_logs());
+
+        // construct again to ensure the queues are empty
+        payload.construct_data();
+
+        // ensure the data is empty
+        raw_data = payload.data();
+        uint8_t empty_buffer[sizeof(TestData)];
+        memset(empty_buffer, 0, sizeof(TestData));
+
+        // ensure the raw data buffer is not empty
+        REQUIRE(memcmp(raw_data, empty_buffer, sizeof(TestData)) == 0);
+    }
+
+    SUBCASE("Testing deconstruct data") {
+        // initialize FirmwareData in hive environment
+        Comms::FirmwareData* firmware_data = new Comms::FirmwareData();
+        Hive::env->firmware_data = firmware_data;
+
+        TestData data1;
+        data1.x = 1.5f;
+        data1.y = 2.5f;
+        data1.z = 3.5f;
+        data1.w = 0x12345678;
+
+        payload.add(new TestData(data1));
+
+        payload.construct_data();
+
+        payload.deconstruct_data(payload.data(), PACKET_SIZE);
+
+        // ensure the data was placed in the correct place in the mega struct
+        REQUIRE(Hive::env->firmware_data->test_data.x == data1.x);
+        REQUIRE(Hive::env->firmware_data->test_data.y == data1.y);
+        REQUIRE(Hive::env->firmware_data->test_data.z == data1.z);
+        REQUIRE(Hive::env->firmware_data->test_data.w == data1.w);
+    }
+
+    SUBCASE("Testing deconstructing full packet") {
+        // initialize FirmwareData in hive environment
+        Comms::FirmwareData* firmware_data = new Comms::FirmwareData();
+        Hive::env->firmware_data = firmware_data;
+
+        TestData data;
+        data.x = 1.5f;
+        data.y = 2.5f;
+        data.z = 3.5f;
+        data.w = 0x12345678;
+
+        Comms::LoggingData logging_data;
+        char log_buffer[] = "This is a very long log message and it probably will be split into multiple packets. Blah";
+        logging_data.deserialize(log_buffer, sizeof(log_buffer));
+
+        payload.add(new TestData(data));
+        payload.add(new Comms::LoggingData(logging_data));
+
+        payload.construct_data();
+
+        payload.deconstruct_data(payload.data(), PACKET_SIZE);
+
+        // ensure the data was placed in the correct place in the mega struct
+        REQUIRE(Hive::env->firmware_data->test_data.x == data.x);
+        REQUIRE(Hive::env->firmware_data->test_data.y == data.y);
+        REQUIRE(Hive::env->firmware_data->test_data.z == data.z);
+        REQUIRE(Hive::env->firmware_data->test_data.w == data.w);
+
+        // store the firmware logging data
+        Comms::LoggingData log_data1;
+        memcpy(&log_data1, &Hive::env->firmware_data->logging_data, sizeof(Comms::LoggingData));
+
+        // construct and deconstruct again to get the rest of the logging data
+        payload.construct_data();
+
+        payload.deconstruct_data(payload.data(), PACKET_SIZE);
+
+        // this data should not have changed
+        REQUIRE(Hive::env->firmware_data->test_data.x == data.x);
+        REQUIRE(Hive::env->firmware_data->test_data.y == data.y);
+        REQUIRE(Hive::env->firmware_data->test_data.z == data.z);
+        REQUIRE(Hive::env->firmware_data->test_data.w == data.w);
+
+        REQUIRE(log_data1.get_logs() + Hive::env->firmware_data->logging_data.get_logs() == logging_data.get_logs());
+    }
+
+    SUBCASE("Testing bad type label deconstruct") {
+        // initialize FirmwareData in hive environment
+        Comms::FirmwareData* firmware_data = new Comms::FirmwareData();
+        Hive::env->firmware_data = firmware_data;
+
+        TestData data;
+        data.x = 1.5f;
+        data.y = 2.5f;
+        data.z = 3.5f;
+        data.w = 0x12345678;
+
+        // set data to have a bad type label
+        data.type_label = static_cast<Comms::TypeLabel>(15);
+
+        // add and construct
+        payload.add(new TestData(data));
+
+        payload.construct_data();
+
+        // deconstruct should throw an error
+        REQUIRE_THROWS_AS(payload.deconstruct_data(payload.data(), PACKET_SIZE), std::runtime_error);
+    }
+
+    SUBCASE("Testing destructor") {
+        Comms::PacketPayload payload(PACKET_SIZE);
+
+        TestData data;
+        data.priority = Comms::Priority::High;
+        payload.add(new TestData(data));
+
+        data.priority = Comms::Priority::Medium;
+        payload.add(new TestData(data));
+
+        data.priority = Comms::Priority::Logging;
+        payload.add(new TestData(data));
+    }
 }
+
+#endif
