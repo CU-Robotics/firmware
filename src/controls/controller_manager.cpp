19ace1b539e236bbcb5a3b48d0ded4a4bd769459
EricV <ericv555@gmail.com>
2025-02-20 23:05:53 -0700

    left a --head in the merge

--- src/controls/controller_manager.cpp
+++ src/controls/controller_manager.cpp
@@ -60,7 +60,7 @@ void ControllerManager::step(float macro_reference[STATE_LEN][3], float macro_es
             if (config_data->controller_info[i][j + 1] < 0) break;
             Serial.printf("\tMotor %d: %f\n", config_data->controller_info[i][j + 1], outputs[j]);
             actuator_write(config_data->controller_info[i][j + 1], outputs[j]);
->>>>>>> origin/main
+
         }
     }
 }
