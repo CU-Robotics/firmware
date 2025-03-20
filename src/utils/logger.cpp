68aaf931bf0a9ad284f84c691acfa9f845ba1401
Rocco Antoniou <roccoantoniou@gmail.com>
2025-03-19 23:14:46 -0600

    logger.cpp/hpp no longer inherit from print. Compiles but printing doesn't work.

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -40,3 +40,157 @@ uint32_t Logger::grab_log_data(uint32_t size, uint8_t* data) {
     // return number of bytes copied
     return bytes_to_copy;
 }
+
+// stuff from teensy4/Print.cpp
+size_t Logger::print(const String& s) {
+    uint8_t buffer[33];
+    size_t count = 0;
+    unsigned int index = 0;
+    unsigned int len = s.length();
+    while (len > 0) {
+        s.getBytes(buffer, sizeof(buffer), index);
+        unsigned int nbytes = len;
+        if (nbytes > sizeof(buffer) - 1) nbytes = sizeof(buffer) - 1;
+        index += nbytes;
+        len -= nbytes;
+        count += write(buffer, nbytes);
+    }
+    return count;
+}
+
+size_t Logger::print(long n) {
+    uint8_t sign = 0;
+
+    if (n < 0) {
+        sign = '-';
+        n = -n;
+    }
+    return printNumber(n, 10, sign);
+}
+
+size_t Logger::print(int64_t n) {
+    if (n < 0) return printNumber64(-n, 10, 1);
+    return printNumber64(n, 10, 0);
+}
+
+size_t Logger::println(void) {
+    uint8_t buf[2] = { '\r', '\n' };
+    return write(buf, 2);
+}
+
+int Logger::printf(const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+
+    char buffer[1024];
+    int retval = vsnprintf_(buffer, 1024, format, ap);
+    write(buffer);
+
+    va_end(ap);
+    return retval;
+}
+
+size_t Logger::printNumber(unsigned long n, uint8_t base, uint8_t sign) {
+    uint8_t buf[34];
+    uint8_t digit, i;
+
+    // TODO: make these checks as inline, since base is
+    // almost always a constant.  base = 0 (BYTE) should
+    // inline as a call directly to write()
+    if (base == 0) {
+        return write((uint8_t)n);
+    } else if (base == 1) {
+        base = 10;
+    }
+
+
+    if (n == 0) {
+        buf[sizeof(buf) - 1] = '0';
+        i = sizeof(buf) - 1;
+    } else {
+        i = sizeof(buf) - 1;
+        while (1) {
+            digit = n % base;
+            buf[i] = ((digit < 10) ? '0' + digit : 'A' + digit - 10);
+            n /= base;
+            if (n == 0) break;
+            i--;
+        }
+    }
+    if (sign) {
+        i--;
+        buf[i] = '-';
+    }
+    return write(buf + i, sizeof(buf) - i);
+}
+
+size_t Logger::printNumber64(uint64_t n, uint8_t base, uint8_t sign) {
+    uint8_t buf[66];
+    uint8_t digit, i;
+
+    if (base < 2) return 0;
+    if (n == 0) {
+        buf[sizeof(buf) - 1] = '0';
+        i = sizeof(buf) - 1;
+    } else {
+        i = sizeof(buf) - 1;
+        while (1) {
+            digit = n % base;
+            buf[i] = ((digit < 10) ? '0' + digit : 'A' + digit - 10);
+            n /= base;
+            if (n == 0) break;
+            i--;
+        }
+    }
+    if (sign) {
+        i--;
+        buf[i] = '-';
+    }
+    return write(buf + i, sizeof(buf) - i);
+}
+
+size_t Logger::printFloat(double number, uint8_t digits) {
+    uint8_t sign = 0;
+    size_t count = 0;
+
+    if (isnan(number)) return print("nan");
+    if (isinf(number)) return print("inf");
+    if (number > 4294967040.0f) return print("ovf");  // constant determined empirically
+    if (number < -4294967040.0f) return print("ovf");  // constant determined empirically
+
+// Handle negative numbers
+    if (number < 0.0) {
+        sign = 1;
+        number = -number;
+    }
+
+    // Round correctly so that print(1.999, 2) prints as "2.00"
+    double rounding = 0.5;
+    for (uint8_t i = 0; i < digits; ++i) {
+        rounding *= 0.1;
+    }
+    number += rounding;
+
+    // Extract the integer part of the number and print it
+    unsigned long int_part = (unsigned long)number;
+    double remainder = number - (double)int_part;
+    count += printNumber(int_part, 10, sign);
+
+    // Print the decimal point, but only if there are digits beyond
+    if (digits > 0) {
+        uint8_t n, buf[16], count = 1;
+        buf[0] = '.';
+
+        // Extract digits from the remainder one at a time
+        if (digits > sizeof(buf) - 1) digits = sizeof(buf) - 1;
+
+        while (digits-- > 0) {
+            remainder *= 10.0;
+            n = (uint8_t)(remainder);
+            buf[count++] = '0' + n;
+            remainder -= n;
+        }
+        count += write(buf, count);
+    }
+    return count;
+}
--- src/utils/logger.hpp
+++ src/utils/logger.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+#include <stdarg.h>
 
 /// @brief comment define statement to stop printing to serial monitor
 #define LOGGER_FLAG
@@ -7,24 +8,37 @@
 /// @brief size of internal buffer
 #define LOGGER_BUFFER_SIZE 4096
 
+enum class LogDestination {
+    Serial,
+    File
+};
+
+#define DEC 10
+#define HEX 16
+#define OCT 8
+#define BIN 2
+
 /// @brief Wrapper for storing print data from Serial
-class Logger :public Print {
+class Logger {
 public:
     /// @brief Default Constructor
-    Logger() = default;
+    Logger() : write_error(0) {}
     /// @brief Default Destructor
     ~Logger() = default;
-
-    /// @brief Neccesary function to utilize Print abstract class
-    /// @param b matches parameters in Print.h
-    /// @return input byte
-    size_t write(uint8_t b) { print("UNEXPECTED PRINT IN LOGGER.HPP"); return b; }
 
     /// @brief copies internal buffer to inputted location (*data) in memory
     /// @return number of bytes copied
     /// @param size of data
     /// @param data pointer to data
     uint32_t grab_log_data(uint32_t size, uint8_t* data);
+
+    /// @brief Implemented to appease the virtual write 
+    /// @param b Arguments match virtual write
+    /// @return not much
+    size_t write(uint8_t b) {
+        Serial.println("Unexpected print in  ");
+        return 0;
+    }
 private:
     /// @brief copies formatted bytes to internal buffer
     /// @return number of bytes
@@ -35,6 +49,122 @@ private:
     /// @brief amount of bytes currently stored in log
     /// @note also used as current position in memory
     uint32_t cursor;
+
+// Stuff from teensy4/print.h
+public:
+    size_t write(const char* str) {
+        if (str == nullptr) return 0;
+        return write((const uint8_t*)str, strlen(str));
+    }
+    virtual int availableForWrite(void) { return 0; }
+    virtual void flush() { }
+    size_t write(const char* buffer, size_t size) { return write((const uint8_t*)buffer, size); }
+    // Print a string
+    size_t print(const String& s);
+    // Print a single character
+    size_t print(char c) { return write((uint8_t)c); }
+    // Print a string
+    size_t print(const char s[ ]) { return write(s); }
+    // Print an unsigned number
+    size_t print(uint8_t b) { return printNumber(b, 10, 0); }
+    // Print a signed number
+    size_t print(int n) { return print((long)n); }
+    // Print an unsigned number
+    size_t print(unsigned int n) { return printNumber(n, 10, 0); }
+    // Print a signed number
+    size_t print(long n);
+    // Print an unsigned number
+    size_t print(unsigned long n) { return printNumber(n, 10, 0); }
+    // Print a signed number
+    size_t print(int64_t n);
+    // Print an unsigned number
+    size_t print(uint64_t n) { return printNumber64(n, 10, 0); }
+
+    // Print a number in any number base (eg, BIN, HEX, OCT)
+    size_t print(unsigned char n, int base) { return printNumber(n, base, 0); }
+    // Print a number in any number base (eg, BIN, HEX, OCT)
+    size_t print(int n, int base) { return (base == 10) ? print(n) : printNumber(n, base, 0); }
+    // Print a number in any number base (eg, BIN, HEX, OCT)
+    size_t print(unsigned int n, int base) { return printNumber(n, base, 0); }
+    // Print a number in any number base (eg, BIN, HEX, OCT)
+    size_t print(long n, int base) { return (base == 10) ? print(n) : printNumber(n, base, 0); }
+    // Print a number in any number base (eg, BIN, HEX, OCT)
+    size_t print(unsigned long n, int base) { return printNumber(n, base, 0); }
+    // Print a number in any number base (eg, BIN, HEX, OCT)
+    size_t print(int64_t n, int base) { return (base == 10) ? print(n) : printNumber64(n, base, 0); }
+    // Print a number in any number base (eg, BIN, HEX, OCT)
+    size_t print(uint64_t n, int base) { return printNumber64(n, base, 0); }
+
+    // Print a floating point (decimal) number
+    size_t print(double n, int digits = 2) { return printFloat(n, digits); }
+    // Print an object instance in human readable format
+    // size_t print(const Printable& obj) { return obj.printTo(*this); }
+    // Print a newline
+    size_t println(void);
+    // Print a string and newline
+    size_t println(const String& s) { return print(s) + println(); }
+    // Print a single character and newline
+    size_t println(char c) { return print(c) + println(); }
+    // Print a string and newline
+    size_t println(const char s[ ]) { return print(s) + println(); }
+    // Print a string and newline
+    size_t println(const __FlashStringHelper* f) { return print(f) + println(); }
+
+    // Print an unsigned number and newline
+    size_t println(uint8_t b) { return print(b) + println(); }
+    // Print a signed number and newline
+    size_t println(int n) { return print(n) + println(); }
+    // Print an unsigned number and newline
+    size_t println(unsigned int n) { return print(n) + println(); }
+    // Print a signed number and newline
+    size_t println(long n) { return print(n) + println(); }
+    // Print an unsigned number and newline
+    size_t println(unsigned long n) { return print(n) + println(); }
+    // Print a signed number and newline
+    size_t println(int64_t n) { return print(n) + println(); }
+    // Print an unsigned number and newline
+    size_t println(uint64_t n) { return print(n) + println(); }
+
+    // Print a number in any number base (eg, BIN, HEX, OCT) and a newline
+    size_t println(unsigned char n, int base) { return print(n, base) + println(); }
+    // Print a number in any number base (eg, BIN, HEX, OCT) and a newline
+    size_t println(int n, int base) { return print(n, base) + println(); }
+    // Print a number in any number base (eg, BIN, HEX, OCT) and a newline
+    size_t println(unsigned int n, int base) { return print(n, base) + println(); }
+    // Print a number in any number base (eg, BIN, HEX, OCT) and a newline
+    size_t println(long n, int base) { return print(n, base) + println(); }
+    // Print a number in any number base (eg, BIN, HEX, OCT) and a newline
+    size_t println(unsigned long n, int base) { return print(n, base) + println(); }
+    // Print a number in any number base (eg, BIN, HEX, OCT) and a newline
+    size_t println(int64_t n, int base) { return print(n, base) + println(); }
+    // Print a number in any number base (eg, BIN, HEX, OCT) and a newline
+    size_t println(uint64_t n, int base) { return print(n, base) + println(); }
+
+    // Print a floating point (decimal) number and a newline
+    size_t println(double n, int digits = 2) { return print(n, digits) + println(); }
+    // Print an object instance in human readable format, and a newline
+    // size_t println(const Printable& obj) { return obj.printTo(*this) + println(); }
+    int getWriteError() { return write_error; }
+    void clearWriteError() { setWriteError(0); }
+
+    // printf is a C standard function which allows you to print any number of variables using a somewhat cryptic format string
+    int printf(const char* format, ...);
+    // printf is a C standard function which allows you to print any number of variables using a somewhat cryptic format string
+    int printf(const __FlashStringHelper* format, ...);
+    // vprintf is a C standard function that allows you to print a variable argument list with a format string
+    int vprintf(const char* format, va_list ap) { return vdprintf((int)this, format, ap); }
+
+    // format warnings are too pedantic - disable until newer toolchain offers better...
+    // https://forum.pjrc.com/threads/62473?p=256873&viewfull=1#post256873
+    // int printf(const char *format, ...) __attribute__ ((format (printf, 2, 3)));
+
+protected:
+    void setWriteError(int err = 1) { write_error = err; }
+private:
+    int write_error;
+    size_t printFloat(double n, uint8_t digits);
+    size_t printNumber(unsigned long n, uint8_t base, uint8_t sign);
+    size_t printNumber64(uint64_t n, uint8_t base, uint8_t sign);
 };
 
 /// @brief universal logger object to be called anywhere in codebase
