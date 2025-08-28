58b0fa005afb19a2641a9a6b050afacc2a9fff1f
Rocco Antoniou <roccoantoniou@gmail.com>
2025-08-28 00:35:46 -0600

    Removed bitwise_operations.*pp

--- src/utils/bitwise_operations.cpp
+++
@@ -1,7 +0,0 @@
-#include "bitwise_operations.hpp"
-
-uint16_t combine_bytes(uint8_t high, uint8_t low) {
-  uint16_t result = 0;
-  return ((result | high) << 8) | low;
-}
-
--- src/utils/bitwise_operations.hpp
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-
-#ifndef BITWISE_OPERATIONS_H
-#define BITWISE_OPERATIONS_H
-
-uint16_t combine_bytes(uint8_t high, uint8_t low);
-
-#endif // BITWISE_OPERATIONS_H
