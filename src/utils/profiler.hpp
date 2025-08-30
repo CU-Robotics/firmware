85b4d4cb736d78245a557d3924a0e8d0978fbe95
rcschwartz18-lgtm <rcschwartz18@gmail.com>
2025-08-30 17:13:19 -0600

    cleared out profiler

--- src/utils/profiler.hpp
+++ src/utils/profiler.hpp
@@ -12,41 +12,6 @@
 
 /// @brief Object for profiling sections of code.
 struct Profiler {
-    /// @brief Constructor for the profiler.
-    Profiler() {
-        clear();
-    }
-
-    /// @brief Data structure for a profiling section. 
-    struct profiler_section_t {
-        /// @brief Start time for each profiling section.
-        uint32_t start_times[PROF_MAX_TIMES] = { 0 };
-        /// @brief End time for each profiling section.
-        uint32_t end_times[PROF_MAX_TIMES] = { 0 };
-        /// @brief Number of start/end times recorded (how many deltas can be calculated).
-        uint16_t count = 0;
-        /// @brief Flag on whether count has overflowed or not
-        uint8_t overflow : 7;
-        /// @brief Flag on whether this section had begin() called on it
-        uint8_t started : 1;
-        /// @brief A unique name to identify the section.
-        char name[PROF_MAX_NAME + 1] = { 0 };  // extra for null terminator
-    };
-
-    /// @brief  Clear all profiling sections.
-    void clear();
-
-    /// @brief Start a profiling section.
-    /// @param name A unique name to identify the section.
-    void begin(const char* name);
-
-    /// @brief End a profiling section.
-    /// @param name The name of the section to end.
-    void end(const char* name);
-
-    /// @brief Print stats for a particular profiling section.
-    /// @param name The name of the section to print statistics for.
-    void print(const char* name);
 };
 
 extern Profiler prof;  // Global profiler
