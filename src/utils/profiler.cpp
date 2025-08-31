9b34d1bba3ffdb24fb518899cec0ec7f6f944d99
rcschwartz18-lgtm <rcschwartz18@gmail.com>
2025-08-31 15:49:37 -0600

    recreated profiler

--- src/utils/profiler.cpp
+++ src/utils/profiler.cpp
@@ -1,2 +1,86 @@
 #include "profiler.hpp"
 
+#ifdef PROFILE
+static DMAMEM Profiler::profiler_section_t sections[PROF_MAX_SECTIONS] = {0};
+#endif
+
+void Profiler::clear() {
+#ifdef PROFILE
+    for(auto& sec : sections)
+        sec = profiler_section_t();
+#endif
+}
+
+void Profiler::begin(const char* name) {
+#ifdef PROFILE
+    //find section by name or first empty slot
+    for(uint32_t i = 0; i < PROF_MAX_SECTIONS; i++) {
+        if(strcmp(sections[i].name, name) == 0 || sections[i].count == 0 && sections[i].overflow == 0) {
+	    //start section
+	    sections[i].name = name;
+	    if(sections[i].count < PROF_MAX_TIMES)
+	    {
+		sections[i].start_times[sections[i].count] = micros();
+	        sections[i].started = 1;//label section as started
+	    }
+	    return;
+	}
+    }
+#endif
+}
+
+void Profiler::end(const char* name) {
+#ifdef PROFILE
+    //find section by name
+    for(uint32_t i = 0; i < PROF_MAX_SECTIONS; i++) {
+	if(strcmp(sections[i].name, name) == 0) {
+	    //end section
+	    if(sections[i].count < PROF_MAX_TIMES) {
+	        sections[i].end_times[count] = micros();
+	        sections[i].count++;
+		sections[i].started = 0;//label section as finished
+		
+		//if count is now at limit, reset count and label as overflowed
+		if(sections[i].count == PROF_MAX_TIMES)
+		{
+		    sections[i].count = 0;
+		    sections[i].overflowed = 1;
+		}
+	    }
+	    return;
+	}
+    }
+#endif
+}
+
+void Profiler::print(const char* name) {
+#ifdef PROFILE
+    uint32_t sum = 0;
+    uint32_t min = UINT32_MAX;
+    uint32_t max = 0;
+    
+    //find section by name
+    for(uint32_t i = 0; i < PROF_MAX_SECTIONS; i++) {
+        if(strcmp(sections[i].name, name) == 0) {
+	    //calculate values
+	    uint32_t trueCount = sections[i].overflowed ? PROF_MAX_SECTIONS : sections[i].count;
+	    for(uint32_t j = 0; j < trueCount; j++) {
+	    	//if the last run was started and not ended, ignore it
+		if(sections[i].started && j == sections[i].count)
+		    continue;
+
+		uint32_t delta = sections[i].end_times[j] - sections[i].end_times[j];
+		sum += delta;
+		if(delta < min)
+		    min = delta;
+		if(delta > max)
+		    max = delta;
+	    }
+
+	    //print values
+	    Serial.printf("Profiling for: %s\n Min: %u us\n Max: %u us\n Avg: %u us", name, min, max, sum/trueCount);
+	    return;
+	}
+    }
+#endif
+}
--- src/utils/profiler.hpp
+++ src/utils/profiler.hpp
@@ -12,6 +12,41 @@
 
 /// @brief Object for profiling sections of code.
 struct Profiler {
+    /// @brief Constructor for the profiler.
+    Profiler() {
+        clear();
+    }
+
+    // @brief Data structure for a profiling section
+    struct profiler_section_t {
+        /// @brief Start time for each profiling section
+	uint32_t start_times[PROF_MAX_TIMES] = {0};
+	/// @brief End time for each profiling section
+	uint32_t end_times[PROF_MAX_TIMES] = {0};
+	/// @brief Number of start/end times recorded
+	uint16_t count = 0;
+	/// @brief Label on if count has overflowed
+	uint8_t overflowed = 0;
+	/// @brief Label on if a begin() has been called and the corresponding end() hasn't yet
+	uint8_t started = 0;
+	/// @brief Name for each section
+	char name[PROF_MAX_NAME] = {0};
+    };
+
+    /// @brief clear all profiling sections
+    void clear();
+
+    /// @brief Start a profiling section
+    /// @param name A unique name to identify the section
+    void begin(const char* name);
+
+    /// @brief End a profiling section
+    /// @param name The name of the section to end
+    void end(const char* name);
+
+    /// @brief Print stats for a profiling section
+    /// @param name The name of the section to print stats for
+    void print(const char* name);
 };
 
 extern Profiler prof;  // Global profiler
