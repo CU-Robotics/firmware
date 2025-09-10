9c945475fadf672ec098c3f47ad667af9b179d8a
rcschwartz18-lgtm <rcschwartz18@gmail.com>
2025-09-09 19:42:39 -0600

    reduced load by only using one array

--- src/utils/profiler.cpp
+++ src/utils/profiler.cpp
@@ -14,40 +14,39 @@ void Profiler::clear() {
 
 void Profiler::begin(const char *name) {
 #ifdef PROFILE
-    // find section by name or first empty slot
-    for (uint32_t i = 0; i < PROF_MAX_SECTIONS; i++) {
-        if (strcmp(sections[i].name, name) == 0 || (sections[i].count == 0 && sections[i].overflowed == 0)) {
-            // start section
-            strncpy(sections[i].name, name, PROF_MAX_NAME - 1);
-            if (sections[i].count < PROF_MAX_TIMES) {
-                sections[i].start_times[sections[i].count] = micros();
-                sections[i].started = 1; // label section as started
-            }
-            return;
-        }
+    //find section by name or first empty slot
+    for(uint32_t i = 0; i < PROF_MAX_SECTIONS; i++) {
+        if(strcmp(sections[i].name, name) == 0 || (sections[i].count == 0 && sections[i].overflowed == 0)) {
+	    //start section
+	    strncpy(sections[i].name, name, PROF_MAX_NAME-1);
+	    sections[i].start_time = micros();
+	    sections[i].started = 1;//label section as started
+	    return;
+	}
     }
 #endif
 }
 
 void Profiler::end(const char *name) {
 #ifdef PROFILE
-    // find section by name
-    for (uint32_t i = 0; i < PROF_MAX_SECTIONS; i++) {
-        if (strcmp(sections[i].name, name) == 0) {
-            // end section
-            if (sections[i].count < PROF_MAX_TIMES) {
-                sections[i].end_times[sections[i].count] = micros();
-                sections[i].count++;
-                sections[i].started = 0; // label section as finished
-
-                // if count is now at limit, reset count and label as overflowed
-                if (sections[i].count == PROF_MAX_TIMES) {
-                    sections[i].count = 0;
-                    sections[i].overflowed = 1;
-                }
-            }
-            return;
-        }
+    //find section by name
+    for(uint32_t i = 0; i < PROF_MAX_SECTIONS; i++) {
+	if(strcmp(sections[i].name, name) == 0) {
+	    //end section
+	    if(sections[i].count < PROF_MAX_TIMES) {
+	        sections[i].time_lengths[sections[i].count] = micros() - sections[i].start_time;
+	        sections[i].count++;
+		sections[i].started = 0;//label section as finished
+		
+		//if count is now at limit, reset count and label as overflowed
+		if(sections[i].count == PROF_MAX_TIMES)
+		{
+		    sections[i].count = 0;
+		    sections[i].overflowed = 1;
+		}
+	    }
+	    return;
+	}
     }
 #endif
 }
@@ -68,13 +67,13 @@ void Profiler::print(const char *name) {
                 if (sections[i].started && j == sections[i].count)
                     continue;
 
-                uint32_t delta = sections[i].end_times[j] - sections[i].start_times[j];
-                sum += delta;
-                if (delta < min)
-                    min = delta;
-                if (delta > max)
-                    max = delta;
-            }
+		uint32_t delta = sections[i].time_lengths[j];
+		sum += delta;
+		if(delta < min)
+		    min = delta;
+		if(delta > max)
+		    max = delta;
+	    }
 
             // print values
             Serial.printf("Profiling for: %s\n Min: %u us\n Max: %u us\n Avg: %u us\n", name, min, max,
