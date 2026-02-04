961f83ad8812738ba8c505d85325635813db8a16
EricV <ericv555@gmail.com>
2026-02-03 20:28:10 -0700

    just a space

--- src/sensors/SensorManager.cpp
+++ src/sensors/SensorManager.cpp
@@ -105,7 +105,6 @@ void SensorManager::read() {
         lidar2->set_yaw(estimated_state[3][0], estimated_state[3][1]);
         lidar2->read();
     }
-
     // read tof sensors
     for (int i = 0; i < tof_sensor_count; i++) {
         // tof_sensors[i]->read();
