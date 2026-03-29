ba7594d4e6d8788d0be75696ddb9e56b647dfbba
leBronzo1 <swisshiss56@gmail.com>
2026-03-28 23:27:26 -0600

    changes to pid tests and new vector tests after refactor more to come from vec math and need to fix sensors

--- test/test_fltrs/test_main.cpp
+++ test/test_fltrs/test_main.cpp
@@ -7,8 +7,7 @@
 void test_wrap_around() {
     PIDFilter pid;
 
-    float gains[4] = {1.0f, 0.0f, 0.0f, 0.0f};
-    pid.set_K(gains);
+    pid.set_gains(1.0f, 0.0f, 0.0f, 0.0f);
 
     pid.setpoint = 0.1f;
     pid.measurement = 2.0f * PI - 0.1f;
@@ -18,33 +17,27 @@ void test_wrap_around() {
     TEST_ASSERT_FLOAT_WITHIN(1e-2f, 0.2f, output);
 }
 
-// Integral test if we ever uncomment it out
-/*void test_integral() {
+void test_proportional() {
     PIDFilter pid;
 
-    float gains[4] = {0.0f, 1.0f, 0.0f, 0.0f}; // Ki = 1
-    pid.set_K(gains);
+    pid.set_gains(2.0f, 0.0f, 0.0f, 0.0f);
 
-    pid.setpoint = 1.0f;
-    pid.measurement = 0.0f; // error = 1
+    pid.setpoint = 3.0f;
+    pid.measurement = 1.0f;
 
-    float out1 = pid.filter(1.0f, false, false);
-    float out2 = pid.filter(1.0f, false, false);
-    float out3 = pid.filter(1.0f, false, false);
+    float output = pid.filter(1.0f, false, false);
 
-    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 1.0f, out1);
-    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 2.0f, out2);
-    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 3.0f, out3);
-}*/
+    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 4.0f, output);
+}
 
 void test_derivative() {
     PIDFilter pid;
 
-    float gains[4] = {0.0f, 0.0f, 1.0f, 0.0f};
-    pid.set_K(gains);
+    pid.set_gains(0.0f, 0.0f, 1.0f, 0.0f);
 
-    pid.setpoint = 0.5f;
-    pid.measurement = 4;
+    pid.setpoint = 0.0f;
+
+    pid.measurement = 4.0f;
     pid.filter(1.0f, false, false);
 
     pid.measurement = 3.0f;
@@ -53,16 +46,61 @@ void test_derivative() {
     TEST_ASSERT_FLOAT_WITHIN(1e-5f, 1.0f, output);
 }
 
+/* If we ever add integral
+void test_integral() {
+    PIDFilter pid;
+
+    pid.set_gains(0.0f, 1.0f, 0.0f, 0.0f);
+
+    pid.setpoint = 1.0f;
+    pid.measurement = 0.0f;
+
+    float output = pid.filter(1.0f, false, false);
+
+    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 1.0f, output);
+}
+*/
+
+
+void test_output_bound() {
+    PIDFilter pid;
+
+    pid.set_gains(10.0f, 0.0f, 0.0f, 0.0f);
+
+    pid.setpoint = 1.0f;
+    pid.measurement = 0.0f;
+
+    float output = pid.filter(1.0f, true, false);
+
+    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 1.0f, output);
+}
+
+void test_feedforward() {
+    PIDFilter pid;
+
+    pid.set_gains(0.0f, 0.0f, 0.0f, 0.5f);
+
+    pid.setpoint = 0.0f;
+    pid.measurement = 0.0f;
+
+    float output = pid.filter(1.0f, false, false);
+
+    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 0.5f, output);
+}
+
 void setup() {
     delay(2000);
 
     UNITY_BEGIN();
+
     RUN_TEST(test_wrap_around);
-    //RUN_TEST(test_integral);
+    RUN_TEST(test_proportional);
     RUN_TEST(test_derivative);
+    RUN_TEST(test_output_bound);
+    RUN_TEST(test_feedforward);
+    //RUN_TEST(test_integral);
+
     UNITY_END();
 }
-
-
 
 void loop() {}
--- test/test_utils/test_main.cpp
+++ test/test_utils/test_main.cpp
@@ -4,15 +4,34 @@
 #include "utils/vector_math.hpp"
 #include "utils/wrapping.hpp"
 
-void test_rotate2d() {
-    float input[2] = {1.0f, 0.0f};
-    float output[2] = {0.0f, 0.0f};
 
-    rotate2D(input, output, 0.0f);
+void test_determinant_3x3() {
+    float mat[3][3] = {
+        {6, 4, 2},
+        {7, 1, 1},
+        {9, 2, 0}
+    };
 
-    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 1.0f, output[0]);
-    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 0.0f, output[1]);
+    float result = determinantOfMatrix(mat);
+
+    TEST_ASSERT_FLOAT_WITHIN(1e-5f, 34.0f, result);
 }
+
+void test_rotate_vector_3d() {
+    float unitVec[3] = {0.0f, 0.0f, 1.0f};
+    float input[3] = {6.0f, 7.0f, 9.0f};
+    float output[3];
+
+    // 90 degs
+    float theta = PI / 2.0f;
+
+    rotateVector3D(unitVec, input, theta, output);
+
+    TEST_ASSERT_FLOAT_WITHIN(1e-3f, -7.0f, output[0]);
+    TEST_ASSERT_FLOAT_WITHIN(1e-3f,  6.0f, output[1]);
+    TEST_ASSERT_FLOAT_WITHIN(1e-3f,  9.0f, output[2]);
+}
+
 
 void test_wrap_angle() {
     TEST_ASSERT_FLOAT_WITHIN(1e-5f, 0.0f, wrapAngle(0.0f));
@@ -24,8 +43,9 @@ void setup() {
     delay(2000);
 
     UNITY_BEGIN();
-    RUN_TEST(test_rotate2d);
     RUN_TEST(test_wrap_angle);
+    RUN_TEST(test_determinant_3x3)
+    RUN_TEST(test_rotate_vector_3d)
     UNITY_END();
 }
 
