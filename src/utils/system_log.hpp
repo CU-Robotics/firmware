6c76bb145433750bf170b78e04fee4d565f5ce1d
EricV <ericv555@gmail.com>
2026-04-21 19:42:30 -0600

    more comments

--- src/utils/system_log.hpp
+++ src/utils/system_log.hpp
@@ -18,7 +18,7 @@ private:
 
     /// @brief A temporary buffer to hold the line while print() is building it
     char current_line[MAX_LINE_LEN] = {0};
-	/// @breif length of print line in buffer
+	/// @brief length of print line in buffer
     uint8_t line_length = 0;
 
     /// @brief handles whether we print to dashboard or direct to serial
@@ -33,8 +33,8 @@ public:
     size_t write(uint8_t c) override;
     
 	/// @brief implements print class print for println,printf,etc...
-	/// @params buffer with message
-	/// @params size of message
+	/// @param *buffer with message
+	/// @param size of message
     size_t write(const uint8_t *buffer, size_t size) override; 
 	/// @brief draws dashboard for live prints from CLI
     void draw_dashboard_box();
