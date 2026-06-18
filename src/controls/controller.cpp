54b255cc00770b0bc5d482197d4472562ead7978
Infantry Jetson <curobotics@colorado.edu>
2026-06-17 22:16:01 -0600

    changes

--- src/controls/controller.cpp
+++ src/controls/controller.cpp
@@ -390,24 +390,30 @@ void FlywheelController::step(RobotStateMap& reference_map, RobotStateMap& estim
 
     float target_motor_velocity = pid_high.filter(dt, false, false) * controller_config.gear_ratios.ball_to_flywheel_rad;
     
-    std::shared_ptr<Motor> flywheel_motors[2] = { flywheel_motor_1, flywheel_motor_2 };
-    float motor_outputs[2];
+    std::shared_ptr<Motor> flywheel_motors[3] = { flywheel_motor_1, flywheel_motor_2, flywheel_motor_3 };
+    float motor_outputs[3];
 
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < num_flywheel_motors; i++) {
         pid_low.kp = low_level_velocity_controller.gains.p;
         pid_low.ki = low_level_velocity_controller.gains.i;
         pid_low.kd = low_level_velocity_controller.gains.d;
         pid_low.kf = low_level_velocity_controller.gains.f;
 
-        pid_low.setpoint = i == 0 ? target_motor_velocity * controller_config.gear_ratios.motor1_direction : 
-                                    target_motor_velocity * controller_config.gear_ratios.motor2_direction;
+        pid_low.setpoint = target_motor_velocity * flywheel_directions[i];
 
-        pid_low.measurement = flywheel_motors[i]->get_state().speed;
+        if (flywheel_motors[i] != nullptr) {
+            pid_low.measurement = flywheel_motors[i]->get_state().speed;
+        } else {
+            pid_low.measurement = 0.0f;
+        }
         motor_outputs[i] = pid_low.filter(dt, true, false);
     }
 
     flywheel_motor_1->write_motor_torque(motor_outputs[0]);
     flywheel_motor_2->write_motor_torque(motor_outputs[1]);
+    if (flywheel_motor_3 != nullptr) {
+        flywheel_motor_3->write_motor_torque(motor_outputs[2]);
+    }
 }
 
 void FlywheelController::handleControllerError(const char* controller_name, const char* state_name, const State& reference_state, const State& estimate_state, float error) {
