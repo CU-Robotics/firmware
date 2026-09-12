9aee692b2b6ec2de367cc84b9164a1f79b2f6aad
EricV <ericv555@gmail.com>
2026-09-12 12:31:56 -0600

    anoteher comment

--- src/utils/system_log.hpp
+++ src/utils/system_log.hpp
@@ -106,6 +106,7 @@ public:
 	/// @brief draws dashboard for live prints from CLI
     void draw_dashboard_box();
     /// @brief Call this once at boot to link the CLI buffer to the logger
+    /// @param pointer to CLI buffer
     void bind_cli_buffer(char* buffer) { shared_cli_buffer = buffer; }
 };
 
